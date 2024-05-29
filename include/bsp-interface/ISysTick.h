76742e547f04d51971993064e4d7397146b492b3
hjc2000 <hjc.666@foxmail.com>
2024-05-29 10:42:37 +0800

    class ISysTick

--- include/bsp-interface/ISysTick.h
+++ include/bsp-interface/ISysTick.h
@@ -1,10 +1,17 @@
 #pragma once
+#include<chrono>
+#include<stdint.h>
 
 namespace bsp
 {
 	class ISysTick
 	{
 	public:
+		virtual uint32_t Frequency() = 0;
 
+		virtual void NopLoopDelayForTicks(uint32_t tick_count) = 0;
+		virtual void NopLoopDelay(std::chrono::microseconds microseconds) = 0;
+		virtual void NopLoopDelay(std::chrono::milliseconds milliseconds) = 0;
+		virtual void NopLoopDelay(std::chrono::seconds seconds) = 0;
 	};
 }
