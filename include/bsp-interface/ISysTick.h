c60f7d166beb84745f8ee306c530dbbd3591bc9a
hjc2000 <hjc.666@foxmail.com>
2024-05-29 10:54:31 +0800

    class ISysTick

--- include/bsp-interface/ISysTick.h
+++ include/bsp-interface/ISysTick.h
@@ -8,10 +8,12 @@ namespace bsp
 	{
 	public:
 		virtual uint32_t Frequency() = 0;
+		virtual uint32_t CurrentValue() = 0;
+		virtual uint32_t ReloadValue() = 0;
 
-		virtual void NopLoopDelayForTicks(uint32_t tick_count) = 0;
-		virtual void NopLoopDelay(std::chrono::microseconds microseconds) = 0;
-		virtual void NopLoopDelay(std::chrono::milliseconds milliseconds) = 0;
-		virtual void NopLoopDelay(std::chrono::seconds seconds) = 0;
+		virtual void NopLoopDelayForTicks(uint32_t tick_count);
+		virtual void NopLoopDelay(std::chrono::microseconds microseconds);
+		virtual void NopLoopDelay(std::chrono::milliseconds milliseconds);
+		virtual void NopLoopDelay(std::chrono::seconds seconds);
 	};
 }
