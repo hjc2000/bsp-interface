cd8646d35d61a44ec553e83a5b8741d202639df2
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-09 14:50:18 +0800

    class ST7789LcdDriver :

--- include/bsp-interface/lcd/ST7789LcdDriver.h
+++ include/bsp-interface/lcd/ST7789LcdDriver.h
@@ -1,4 +1,5 @@
 #pragma once
+#include<bsp-interface/IDelayer.h>
 #include<bsp-interface/lcd/ILcd.h>
 
 namespace bsp
@@ -6,6 +7,76 @@ namespace bsp
 	class ST7789LcdDriver :
 		public bsp::ILcd
 	{
+	private:
+		bsp::IDelayer &_delayer;
+		bool _is_horizontal_priority_scanning = true;
+		bsp::ILcd::HorizontalDirection _horizontal_direction = bsp::ILcd::HorizontalDirection::LeftToRight;
+		bsp::ILcd::VerticalDirection _vertical_direction = bsp::ILcd::VerticalDirection::TopToBottom;
+
+	protected:
+		ST7789LcdDriver(bsp::IDelayer &delayer) :
+			_delayer(delayer)
+		{
+
+		}
+
+		/// <summary>
+		///		屏幕初始宽度。与屏幕旋转无关，无论屏幕怎么旋转，本属性都保持不变。
+		/// </summary>
+		/// <returns></returns>
+		static consteval uint32_t OriginalWidth()
+		{
+			return 240;
+		}
+
+		/// <summary>
+		///		屏幕初始高度。与屏幕旋转无关，无论屏幕怎么旋转，本属性都保持不变。
+		/// </summary>
+		/// <returns></returns>
+		static consteval uint32_t OriginHeight()
+		{
+			return 320;
+		}
+
+		/// <summary>
+		///		屏幕总共有多少个点。
+		/// </summary>
+		/// <returns></returns>
+		static consteval uint32_t PointCount()
+		{
+			return OriginalWidth() * OriginHeight();
+		}
+
+		uint16_t ColorCode(bsp::Color color)
+		{
+			switch (color)
+			{
+			case bsp::Color::Red:
+				{
+					return 0x001F;
+				}
+			case bsp::Color::Green:
+				{
+					return 0x07E0;
+				}
+			case bsp::Color::Blue:
+				{
+					return 0xF800;
+				}
+			case bsp::Color::White:
+				{
+					// 0xffff 表示该像素的 3 个液晶全部透光度开到最大，呈现出白色
+					return UINT16_MAX;
+				}
+			case bsp::Color::Black:
+			default:
+				{
+					// 0 表示全不透光，所以是黑色
+					return 0;
+				}
+			}
+		}
+
 		#pragma region 接口
 	public:
 		virtual void WriteCommand(uint16_t cmd) = 0;
@@ -24,6 +95,21 @@ namespace bsp
 		void DisplayOn() override;
 		void DisplayOff() override;
 
+		/// <summary>
+		///		设置扫描方向。
+		/// </summary>
+		/// <note>
+		///		让 LCD 显示的方法是将屏幕上的一个个像素的 RGB 值逐个写入。
+		///		有多少个像素，就要写入多少组 RGB 值。写入一个像素的 RGB 值
+		///		后，驱动芯片会自动将位置指针递增，下次写入 RGB 就是对应下一个像素了。
+		/// 
+		///		驱动芯片内部自动递增的指针就是扫描指针，指向的是当前要被写入的像素。
+		///		扫描顺序可以是先水平，后竖直，也可以是先竖直，后水平。水平又可以分为
+		///		从左到右和从右到左。竖直可以分为从上到下和从下到上。
+		/// </note>
+		/// <param name="horizontal_priority_scanning"></param>
+		/// <param name="hdir"></param>
+		/// <param name="vdir"></param>
 		void SetScanDirection(
 			bool horizontal_priority_scanning,
 			bsp::ILcd::HorizontalDirection hdir,
