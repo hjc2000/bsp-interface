4b047a7345cca3515f90c390fa60fad1ee5e8ffc
hjc2000 <hjc.666@foxmail.com>
2024-06-11 14:57:27 +0800

    class ITimerInterruptManager

--- include/bsp-interface/timer/ITimerInterruptManager.h
+++ include/bsp-interface/timer/ITimerInterruptManager.h
@@ -13,8 +13,10 @@ namespace bsp
 	public:
 		#pragma region 接口
 		/// <summary>
-		///		本类用户禁止通过本函数直接获取 map 来获得回调函数。
-		///		必须使用 GetTimerIrqHandlerFromIsr 函数。
+		///		本类用户禁止通过本函数直接获取 map 来获得回调函数。必须使用 GetTimerIrqHandlerFromIsr 函数。
+		///		* 本函数是供本类内部使用，用户需要实现，实现后就不要去自己调用它。
+		///		* 如果是 A 类，B 类都实现了本接口，B 类将 A 类作为字段，此时 B 类的 HandleMap 函数实现可以
+		///		  是转发 A 类的，这是合法的，除此以外，不要调用本函数。
 		/// </summary>
 		/// <returns></returns>
 		virtual std::map<uint32_t, std::function<void()>> &HandleMap() = 0;
