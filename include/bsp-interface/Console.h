f833246002089c2e508750f78b1e00de27549de5
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-16 22:22:36 +0800

    void WriteError(char const *str) noexcept;

--- include/bsp-interface/Console.h
+++ include/bsp-interface/Console.h
@@ -1,13 +1,11 @@
 #pragma once
-#include<base/stream/Stream.h>
-#include<memory>
-#include<string>
+#include <base/stream/Stream.h>
+#include <memory>
+#include <string>
 
 namespace bsp
 {
-	/// <summary>
-	///		单例控制台
-	/// </summary>
+	/// @brief 单例控制台
 	class Console
 	{
 	private:
@@ -27,6 +25,10 @@ namespace bsp
 		}
 
 		std::shared_ptr<base::Stream> OutStream();
+
+		/// @brief 为本控制台设置用来输出的输出流。
+		/// @note 如果没有设置过，则输出流为空，无法进行输出。
+		/// @param value
 		void SetOutStream(std::shared_ptr<base::Stream> value);
 
 		void Write(std::string const &str);
@@ -34,16 +36,12 @@ namespace bsp
 		void Write(char const *str);
 		void WriteLine(char const *str);
 
-		/// <summary>
-		///		错误汇报。
-		///		* 尝试利用串口输出错误信息。
-		///		* 本函数不会抛出异常，内部捕获了所有异常，这确保当串口不可用，
-		///		  串口类抛出异常时不会导致异常逃逸。因为 WriteError 是设计
-		///		  为在 catch 块中使用的，而 catch 块一般不再嵌套 try-catch。
-		///		* 严重错误时可能会连本函数都用不了。
-		/// </summary>
-		/// <param name="str"></param>
+		/// @brief 错误汇报。
+		/// @param str
 		void WriteError(char const *str) noexcept;
+
+		/// @brief 错误汇报。
+		/// @param str
 		void WriteError(std::string const &str) noexcept;
 	};
 }
