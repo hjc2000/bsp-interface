87d4c76de9ef89b800df44e171db0401a90dd2fd
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-27 00:40:31 +0800

    注释

--- include/bsp-interface/IIndependentWatchDog.h
+++ include/bsp-interface/IIndependentWatchDog.h
@@ -1,30 +1,22 @@
 #pragma once
-#include<chrono>
+#include <chrono>
 
 namespace bsp
 {
-	/// <summary>
-	///		独立看门狗接口。
-	/// </summary>
+	/// @brief 独立看门狗接口。
 	class IIndependentWatchDog
 	{
 	public:
-		/// <summary>
-		///		看门狗超时时间。
-		/// </summary>
-		/// <returns></returns>
+		/// @brief 看门狗超时时间。
+		/// @return
 		virtual std::chrono::milliseconds WatchDogTimeoutDuration() const = 0;
 
-		/// <summary>
-		///		设置看门狗超时时间。
-		///		* 如果设置的毫秒数超出了能力范围，则会使用能达到的最大计时时间。
-		/// </summary>
-		/// <param name="value"></param>
+		/// @brief 设置看门狗超时时间。
+		/// @note 如果设置的毫秒数超出了能力范围，则会使用能达到的最大计时时间。
+		/// @param value
 		virtual void SetWatchDogTimeoutDuration(std::chrono::milliseconds value) = 0;
 
-		/// <summary>
-		///		喂狗。
-		/// </summary>
+		/// @brief 喂狗
 		virtual void Feed() = 0;
 	};
 }
