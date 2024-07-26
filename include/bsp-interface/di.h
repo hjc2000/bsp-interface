462244006856e1ec247c07e4d98f31c29e6a0681
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-27 00:45:54 +0800

    bsp::IKeyScanner &DI_KeyScanner();

--- include/bsp-interface/di.h
+++ include/bsp-interface/di.h
@@ -3,8 +3,10 @@
 #include <bsp-interface/ISysTick.h>
 #include <bsp-interface/interrupt/IInterruptSwitch.h>
 #include <bsp-interface/interrupt/IsrManager.h>
+#include <bsp-interface/key/IKeyScanner.h>
 
 bsp::ISysTick &DI_SysTick();
 bsp::IDelayer &DI_Delayer();
 bsp::IInterruptSwitch &DI_InterruptSwitch();
 bsp::IsrManager &DI_IsrManager();
+bsp::IKeyScanner &DI_KeyScanner();
--- include/bsp-interface/key/IKeyScanner.h
+++ include/bsp-interface/key/IKeyScanner.h
@@ -1,18 +1,18 @@
 #pragma once
-#include<bsp-interface/key/IKey.h>
-#include<stdint.h>
+#include <bsp-interface/key/IKey.h>
+#include <stdint.h>
 
 namespace bsp
 {
+	/// @brief 按键扫描器接口
+	/// @note 要求调用一次本函数后，内部储存扫描结果，接下来无论多少次检查按键事件，
+	/// 结果都一样，即检查事件的函数是幂等的。只有再次调用本函数，按键事件的有无
+	/// 才会被更新。
 	class IKeyScanner
 	{
 	public:
-		/// <summary>
-		///		* 此函数需要被循环调用，这样才能扫描到按键事件。
-		///		* 要求调用一次本函数后，内部储存扫描结果，接下来无论多少次检查按键事件，
-		///		  结果都一样，即检查事件的函数是幂等的。只有再次调用本函数，按键事件的有无
-		///		  才会被更新。
-		/// </summary>
+		/// @brief 扫描按键
+		/// @note 此函数需要被循环调用，这样才能扫描到按键事件。
 		virtual void ScanKeys() = 0;
 
 		virtual bool HasKeyDownEvent(uint16_t key_index) = 0;
