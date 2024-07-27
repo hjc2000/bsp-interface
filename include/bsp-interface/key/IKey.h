5ccf0c665cf551f945463967d935a67e8d2a39ca
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-27 13:05:34 +0800

    注释

--- include/bsp-interface/key/IKey.h
+++ include/bsp-interface/key/IKey.h
@@ -1,23 +1,20 @@
 #pragma once
-#include<bitset>
-#include<chrono>
+#include <bitset>
+#include <chrono>
 
 namespace bsp
 {
-	/// <summary>
-	///		独立按键。
-	///		- 所谓独立按键就是一个按键占用一个 GPIO 引脚。
-	/// </summary>
+	/// @brief 按键接口。
 	class IKey
 	{
 	public:
-		/// <summary>
-		///		按键处于被按下的状态。
-		///		此函数内部不会进行按键消抖，抖动会引起此函数返回值的变化。
-		/// </summary>
-		/// <returns>按键处于被按下的状态则返回 true，按键不处于被按下的状态则返回 false。</returns>
+		/// @brief 按键处于被按下的状态。
+		/// @note 此函数内部不会进行按键消抖，抖动会引起此函数返回值的变化。
+		/// @return 按键处于被按下的状态则返回 true，按键不处于被按下的状态则返回 false。
 		virtual bool KeyIsDown() = 0;
 
+		/// @brief 按键处于松开的状态。
+		/// @return 处于松开的状态返回 true，不处于松开的状态返回 false。
 		bool KeyIsUp()
 		{
 			return !KeyIsDown();
