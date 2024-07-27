5e502fe73737c91a7ae0161d4509d3a9c0b79efd
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-28 00:14:02 +0800

    /// @brief 数字 LED 灯集合。 /// @return base::IReadOnlyCollection<int, bsp::IDigitalLed *> &DI_DigitalLedCollection();

--- include/bsp-interface/di.h
+++ include/bsp-interface/di.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <base/container/ICollection.h>
 #include <bsp-interface/IDelayer.h>
+#include <bsp-interface/IDigitalLed.h>
 #include <bsp-interface/ISysTick.h>
 #include <bsp-interface/interrupt/IInterruptSwitch.h>
 #include <bsp-interface/interrupt/IsrManager.h>
@@ -22,3 +23,7 @@ bsp::IsrManager &DI_IsrManager();
 /// @return
 base::IReadOnlyCollection<int, bsp::IKey *> &DI_KeyCollection();
 bsp::IKeyScanner &DI_KeyScanner();
+
+/// @brief 数字 LED 灯集合。
+/// @return
+base::IReadOnlyCollection<int, bsp::IDigitalLed *> &DI_DigitalLedCollection();
