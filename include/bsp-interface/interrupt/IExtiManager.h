a8d00970ccf51c0cc93e10a6842bc3c3ec4c3e06
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-28 20:22:07 +0800

    line_id

--- include/bsp-interface/interrupt/IExtiManager.h
+++ include/bsp-interface/interrupt/IExtiManager.h
@@ -9,12 +9,12 @@ namespace bsp
 	{
 	public:
 		/// @brief 注册使用一条外部中断线。
-		/// @param line_index
+		/// @param line_id 中断线的 id
 		/// @param callback 发生中断时的回调函数
-		virtual void Register(int line_index, std::function<void()> callback) = 0;
+		virtual void Register(int line_id, std::function<void()> callback) = 0;
 
 		/// @brief 注销一条外部中断线的使用。
-		/// @param line_index
-		virtual void Unregister(int line_index) = 0;
+		/// @param line_id 中断线的 id
+		virtual void Unregister(int line_id) = 0;
 	};
 }
