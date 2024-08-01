149a76f6414ba9a44b65aca75fa86eba6a3abf4d
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-01 22:14:17 +0800

    class ISerial

--- include/bsp-interface/ISerial.cpp
+++ include/bsp-interface/ISerial.cpp
@@ -1,13 +1,13 @@
-#include"ISerial.h"
+#include "ISerial.h"
 
 using namespace bsp;
 
 uint32_t bsp::ISerial::CalculateFramesBaudCount(uint32_t frame_count)
 {
 	uint32_t baud_count = 0;
-	baud_count += 1 * frame_count; // 1 位起始位
+	baud_count += 1 * frame_count;			// 1 位起始位
 	baud_count += DataBits() * frame_count; // 数据位
-	if (Parity() != ParityOption::None)
+	if (Parity() != bsp::ISerialParity::None)
 	{
 		baud_count += 1 * frame_count; // 1 位校验位
 	}
@@ -15,43 +15,43 @@ uint32_t bsp::ISerial::CalculateFramesBaudCount(uint32_t frame_count)
 	// 停止位
 	switch (StopBits())
 	{
-	case StopBitsOption::ZeroPointFive:
+	case bsp::ISerialStopBits::ZeroPointFive:
+	{
+		// 每 2 个帧占用 1 个位。
+		baud_count += frame_count / 2;
+
+		// 还剩余半个位，当成 1 位。
+		if (frame_count % 2 > 0)
 		{
-			// 每 2 个帧占用 1 个位。
-			baud_count += frame_count / 2;
-
-			// 还剩余半个位，当成 1 位。
-			if (frame_count % 2 > 0)
-			{
-				baud_count += 1;
-			}
-
-			break;
+			baud_count += 1;
 		}
+
+		break;
+	}
 	default:
-	case StopBitsOption::One:
-		{
-			baud_count += 1 * frame_count;
-			break;
-		}
-	case StopBitsOption::OnePointFive:
-		{
-			// 每 2 个帧占用 3 个位。
-			baud_count += (frame_count / 2) * 3;
+	case bsp::ISerialStopBits::One:
+	{
+		baud_count += 1 * frame_count;
+		break;
+	}
+	case bsp::ISerialStopBits::OnePointFive:
+	{
+		// 每 2 个帧占用 3 个位。
+		baud_count += (frame_count / 2) * 3;
 
-			// 还剩余 1.5 个位，当成 2 位。
-			if (frame_count % 2 > 0)
-			{
-				baud_count += 2;
-			}
-
-			break;
-		}
-	case StopBitsOption::Tow:
+		// 还剩余 1.5 个位，当成 2 位。
+		if (frame_count % 2 > 0)
 		{
-			baud_count += 2 * frame_count;
-			break;
+			baud_count += 2;
 		}
+
+		break;
+	}
+	case bsp::ISerialStopBits::Tow:
+	{
+		baud_count += 2 * frame_count;
+		break;
+	}
 	}
 
 	return baud_count;
--- include/bsp-interface/ISerial.h
+++ include/bsp-interface/ISerial.h
@@ -1,87 +1,80 @@
 #pragma once
-#include<base/stream/Stream.h>
+#include <base/stream/Stream.h>
 
 namespace bsp
 {
-	class ISerial :
-		public base::Stream
+	/// @brief 用来设置 ISerial 的校验方式
+	enum class ISerialParity
+	{
+		None,
+		Even,
+		Odd,
+	};
+
+	/// @brief 用来设置 ISerial 的停止位位数。
+	enum class ISerialStopBits
+	{
+		/// @brief 0.5
+		ZeroPointFive,
+
+		/// @brief 1
+		One,
+
+		/// @brief 1.5
+		OnePointFive,
+
+		/// @brief 2
+		Tow,
+	};
+
+	/// @brief 用来设置 ISerial 的硬件流控。
+	enum class ISerialHardwareFlowControl
+	{
+		None,
+		RTS,
+		CTS,
+		RTS_CTS,
+	};
+
+	/// @brief 串口接口。
+	class ISerial
+		: public base::Stream
 	{
 	public:
-		/// <summary>
-		///		波特率。
-		/// </summary>
-		/// <returns></returns>
+		/// @brief 波特率。
+		/// @return
 		virtual uint32_t BaudRate() const = 0;
 		virtual void SetBaudRate(uint32_t value) = 0;
 
-		/// <summary>
-		///		数据位的个数。
-		/// </summary>
-		/// <returns></returns>
+		/// @brief 数据位的个数。
+		/// @return
 		virtual uint8_t DataBits() const = 0;
 		virtual void SetDataBits(uint8_t value) = 0;
 
-		enum class ParityOption
-		{
-			None,
-			Even,
-			Odd,
-		};
+		/// @brief 校验位。
+		/// @return
+		virtual bsp::ISerialParity Parity() const = 0;
+		virtual void SetParity(bsp::ISerialParity value) = 0;
 
-		/// <summary>
-		///		校验位。
-		/// </summary>
-		/// <returns></returns>
-		virtual bsp::ISerial::ParityOption Parity() const = 0;
-		virtual void SetParity(bsp::ISerial::ParityOption value) = 0;
+		/// @brief 停止位个数。
+		/// @return
+		virtual bsp::ISerialStopBits StopBits() const = 0;
+		virtual void SetStopBits(bsp::ISerialStopBits value) = 0;
 
-		enum class StopBitsOption
-		{
-			ZeroPointFive, // 0.5
-			One, // 1
-			OnePointFive, // 1.5
-			Tow, // 2
-		};
+		/// @brief 硬件流控。
+		/// @return
+		virtual bsp::ISerialHardwareFlowControl HardwareFlowControl() const = 0;
+		virtual void SetHardwareFlowControl(bsp::ISerialHardwareFlowControl value) = 0;
 
-		/// <summary>
-		///		停止位个数。
-		/// </summary>
-		/// <returns></returns>
-		virtual bsp::ISerial::StopBitsOption StopBits() const = 0;
-		virtual void SetStopBits(bsp::ISerial::StopBitsOption value) = 0;
-
-		/// <summary>
-		///		硬件流控。
-		/// </summary>
-		enum class HardwareFlowControlOption
-		{
-			None,
-			RTS,
-			CTS,
-			RTS_CTS,
-		};
-
-		/// <summary>
-		///		硬件流控。
-		/// </summary>
-		/// <returns></returns>
-		virtual bsp::ISerial::HardwareFlowControlOption HardwareFlowControl() const = 0;
-		virtual void SetHardwareFlowControl(bsp::ISerial::HardwareFlowControlOption value) = 0;
-
-		/// <summary>
-		///		以指定的波特率启动串口。
-		/// </summary>
-		/// <param name="baud_rate"></param>
+		/// @brief 以指定的波特率启动串口。
 		virtual void Open() = 0;
 
-		/// <summary>
-		///		计算 frame_count 个帧占用多少个波特。
-		/// </summary>
-		/// <param name="frame_count"></param>
-		/// <returns></returns>
+		/// @brief 计算 frame_count 个帧占用多少个波特。
+		/// @param frame_count
+		/// @return
 		uint32_t CalculateFramesBaudCount(uint32_t frame_count);
 
-		#pragma region Stream
+#pragma region Stream
 		bool CanRead() override;
 		bool CanWrite() override;
 		bool CanSeek() override;
@@ -93,7 +86,6 @@ namespace bsp
 
 		int64_t Position() override;
 		void SetPosition(int64_t value) override;
-		#pragma endregion
-
+#pragma endregion
 	};
 }
