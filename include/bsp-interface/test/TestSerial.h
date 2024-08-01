70bdc83690b73f97ad6e74b4906c252a4af69604
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-02 00:26:04 +0800

    inline void TestSerial()

---
+++ include/bsp-interface/test/TestSerial.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <bsp-interface/di.h>
+
+namespace bsp
+{
+	inline void TestSerial()
+	{
+		DI_RedDigitalLed().TurnOn();
+		DI_Serial().Open(*DICreate_ISerialOptions());
+		std::unique_ptr<uint8_t[]> buffer{new uint8_t[128]};
+		while (1)
+		{
+			int32_t have_read = DI_Serial().Read(buffer.get(), 0, 128);
+			DI_GreenDigitalLed().Toggle();
+			DI_Serial().Write(buffer.get(), 0, have_read);
+		}
+	}
+
+}
