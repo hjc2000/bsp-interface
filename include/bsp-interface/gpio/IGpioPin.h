3566b44072ada2f2de0e0bab8ffb8a8e59a7a884
hjc2000 <hjc.666@foxmail.com>
2024-08-02 09:14:30 +0800

    class IGpioPinOptions

--- include/bsp-interface/gpio/IGpioPin.h
+++ include/bsp-interface/gpio/IGpioPin.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <base/container/IEnumerable.h>
-#include <bsp-interface/gpio/GpioPinPullMode.h>
-#include <bsp-interface/gpio/GpioPinWorkMode.h>
+#include <bsp-interface/gpio/IGpioPinOptions.h>
 #include <functional>
 #include <string>
 
@@ -30,32 +29,6 @@ namespace bsp
 		/// @return
 		virtual bool IsOpen() = 0;
 
-		/// @brief 引脚工作模式。
-		/// @return
-		virtual GpioPinWorkMode WorkMode() const = 0;
-		virtual void SetWorkMode(GpioPinWorkMode value) = 0;
-
-		/// @brief 引脚上下拉模式。
-		/// @return
-		virtual GpioPinPullMode PullMode() const = 0;
-		virtual void SetPullMode(GpioPinPullMode value) = 0;
-
-		/// @brief 引脚速度等级。
-		/// @note 有的平台引脚支持设置速度等级。不支持的平台忽略就好。
-		/// @return
-		virtual int SpeedLevel() const = 0;
-		virtual void SetSpeedLevel(int value) = 0;
-
-		/// @brief 复用功能。
-		/// @return
-		virtual std::string AlternateFunction() const = 0;
-
-		/// @brief 设置复用功能。
-		/// @note 复用功能是非常复杂的，各个型号单片机都不一样，所以采用字符串。
-		/// @note 假设某个单片机的一个引脚的复用功能有：uart1, timer1_compare_output 等。
-		/// 这种名称是与具体型号高度相关的，所以本库无法提供一个枚举来列举这些情况。
-		/// @param value
-		virtual void SetAlternateFunction(std::string value) = 0;
 #pragma endregion
 
 		/// @brief 支持的复用功能。
---
+++ include/bsp-interface/gpio/IGpioPinOptions.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <string>
+
+namespace bsp
+{
+	/// @brief 引脚工作模式。
+	enum class IGpioPinWorkMode
+	{
+		/// @brief 输入
+		Input,
+
+		/// @brief 输入中断模式。上升沿触发。
+		InputInterruptRisingEdgeTrigger,
+
+		/// @brief 输入中断模式。下降沿触发。
+		InputInterruptFallingEdgeTrigger,
+
+		/// @brief 输入中断模式。上升沿和下降沿都可触发。
+		InputInterruptBothEdgeTrigger,
+
+		/// @brief 推挽输出
+		OutputPushPull,
+
+		/// @brief 开漏输出
+		OutputOpenDrain,
+
+		/// @brief 复用功能
+		AlternateFunction,
+	};
+
+	/// @brief 引脚上下拉模式。
+	enum class IGpioPinPullMode
+	{
+		NoPull,
+		PullUp,
+		PullDown,
+	};
+
+	/// @brief GPIO 引脚选项
+	class IGpioPinOptions
+	{
+	public:
+		/// @brief 引脚工作模式。
+		/// @return
+		virtual IGpioPinWorkMode WorkMode() const = 0;
+		virtual void SetWorkMode(IGpioPinWorkMode value) = 0;
+
+		/// @brief 引脚上下拉模式。
+		/// @return
+		virtual IGpioPinPullMode PullMode() const = 0;
+		virtual void SetPullMode(IGpioPinPullMode value) = 0;
+
+		/// @brief 引脚速度等级。
+		/// @note 有的平台引脚支持设置速度等级。不支持的平台忽略就好。
+		/// @return
+		virtual int SpeedLevel() const = 0;
+		virtual void SetSpeedLevel(int value) = 0;
+
+		/// @brief 复用功能。
+		/// @return
+		virtual std::string AlternateFunction() const = 0;
+
+		/// @brief 设置复用功能。
+		/// @note 复用功能是非常复杂的，各个型号单片机都不一样，所以采用字符串。
+		/// @note 假设某个单片机的一个引脚的复用功能有：uart1, timer1_compare_output 等。
+		/// 这种名称是与具体型号高度相关的，所以本库无法提供一个枚举来列举这些情况。
+		/// @param value
+		virtual void SetAlternateFunction(std::string value) = 0;
+	};
+}
