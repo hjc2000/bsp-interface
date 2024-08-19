e9aa287cf93388a720f54db3f8ef1b22ce1c0d2a
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-19 23:58:27 +0800

    uint32_t CalculateFramesBaudCount(uint32_t frame_count) const;

--- include/bsp-interface/serial/ISerial.cpp
+++ include/bsp-interface/serial/ISerial.cpp
@@ -2,99 +2,46 @@
 
 using namespace bsp;
 
-uint32_t bsp::ISerialOptions::CalculateFramesBaudCount(uint32_t frame_count)
-{
-	uint32_t baud_count = 0;
-	baud_count += 1 * frame_count;			// 1 位起始位
-	baud_count += DataBits() * frame_count; // 数据位
-	if (Parity() != bsp::ISerialParity::None)
-	{
-		baud_count += 1 * frame_count; // 1 位校验位
-	}
-
-	// 停止位
-	switch (StopBits())
-	{
-	case bsp::ISerialStopBits::ZeroPointFive:
-	{
-		// 每 2 个帧占用 1 个位。
-		baud_count += frame_count / 2;
-
-		// 还剩余半个位，当成 1 位。
-		if (frame_count % 2 > 0)
-		{
-			baud_count += 1;
-		}
-
-		break;
-	}
-	default:
-	case bsp::ISerialStopBits::One:
-	{
-		baud_count += 1 * frame_count;
-		break;
-	}
-	case bsp::ISerialStopBits::OnePointFive:
-	{
-		// 每 2 个帧占用 3 个位。
-		baud_count += (frame_count / 2) * 3;
-
-		// 还剩余 1.5 个位，当成 2 位。
-		if (frame_count % 2 > 0)
-		{
-			baud_count += 2;
-		}
-
-		break;
-	}
-	case bsp::ISerialStopBits::Tow:
-	{
-		baud_count += 2 * frame_count;
-		break;
-	}
-	}
-
-	return baud_count;
-}
-
 #pragma region Stream
+
 bool ISerial::CanRead()
 {
-	return true;
+    return true;
 }
 
 bool ISerial::CanWrite()
 {
-	return true;
+    return true;
 }
 
 bool ISerial::CanSeek()
 {
-	return false;
+    return false;
 }
 
 int64_t ISerial::Length()
 {
-	return 0;
+    return 0;
 }
 
 void ISerial::SetLength(int64_t value)
 {
-	// 不支持的操作
+    // 不支持的操作
 }
 
 void ISerial::Flush()
 {
-	// Write 方法利用 DMA 直接发送缓冲区，本类没有内部缓冲区，不需要冲洗。
+    // Write 方法利用 DMA 直接发送缓冲区，本类没有内部缓冲区，不需要冲洗。
 }
 
 int64_t ISerial::Position()
 {
-	return 0;
+    return 0;
 }
 
 void ISerial::SetPosition(int64_t value)
 {
-	// 不支持的操作。
+    // 不支持的操作。
 }
+
 #pragma endregion
--- include/bsp-interface/serial/ISerialOptions.h
+++ include/bsp-interface/serial/ISerialOptions.h
@@ -3,94 +3,94 @@
 
 namespace bsp
 {
-	/// @brief 用来设置 ISerial 的校验方式
-	enum class ISerialParity
-	{
-		/// @brief 不校验。
-		None,
+    /// @brief 用来设置 ISerial 的校验方式
+    enum class ISerialParity
+    {
+        /// @brief 不校验。
+        None,
 
-		/// @brief 偶校验。
-		Even,
+        /// @brief 偶校验。
+        Even,
 
-		/// @brief 奇校验。
-		Odd,
-	};
+        /// @brief 奇校验。
+        Odd,
+    };
 
-	/// @brief 用来设置 ISerial 的停止位位数。
-	enum class ISerialStopBits
-	{
-		/// @brief 0.5
-		ZeroPointFive,
+    /// @brief 用来设置 ISerial 的停止位位数。
+    enum class ISerialStopBits
+    {
+        /// @brief 0.5
+        ZeroPointFive,
 
-		/// @brief 1
-		One,
+        /// @brief 1
+        One,
 
-		/// @brief 1.5
-		OnePointFive,
+        /// @brief 1.5
+        OnePointFive,
 
-		/// @brief 2
-		Tow,
-	};
+        /// @brief 2
+        Tow,
+    };
 
-	/// @brief 用来设置 ISerial 的硬件流控。
-	enum class ISerialHardwareFlowControl
-	{
-		None,
-		RTS,
-		CTS,
-		RTS_CTS,
-	};
+    /// @brief 用来设置 ISerial 的硬件流控。
+    enum class ISerialHardwareFlowControl
+    {
+        None,
+        RTS,
+        CTS,
+        RTS_CTS,
+    };
 
-	/// @brief 串口数据传输方向。
-	enum class ISerialDirection
-	{
-		RX,
-		TX,
-		RX_TX,
-	};
+    /// @brief 串口数据传输方向。
+    enum class ISerialDirection
+    {
+        RX,
+        TX,
+        RX_TX,
+    };
 
-	/// @brief 串口选项。
-	class ISerialOptions
-	{
-	public:
-		virtual ~ISerialOptions() = default;
+    /// @brief 串口选项。
+    class ISerialOptions
+    {
+    public:
+        virtual ~ISerialOptions() = default;
 
-		/// @brief 将本对象设置为默认配置。
-		virtual void UseDefaultOptions() = 0;
+        /// @brief 将本对象设置为默认配置。
+        virtual void UseDefaultOptions() = 0;
 
-		/// @brief 数据传输方向
-		/// @return
-		virtual bsp::ISerialDirection Direction() = 0;
-		virtual void SetDirection(bsp::ISerialDirection value) = 0;
+        /// @brief 数据传输方向
+        /// @return
+        virtual bsp::ISerialDirection Direction() = 0;
+        virtual void SetDirection(bsp::ISerialDirection value) = 0;
 
-		/// @brief 波特率。
-		/// @return
-		virtual uint32_t BaudRate() const = 0;
-		virtual void SetBaudRate(uint32_t value) = 0;
+        /// @brief 波特率。
+        /// @return
+        virtual uint32_t BaudRate() const = 0;
+        virtual void SetBaudRate(uint32_t value) = 0;
 
-		/// @brief 数据位的个数。
-		/// @return
-		virtual uint8_t DataBits() const = 0;
-		virtual void SetDataBits(uint8_t value) = 0;
+        /// @brief 数据位的个数。
+        /// @return
+        virtual uint8_t DataBits() const = 0;
+        virtual void SetDataBits(uint8_t value) = 0;
 
-		/// @brief 校验位。
-		/// @return
-		virtual bsp::ISerialParity Parity() const = 0;
-		virtual void SetParity(bsp::ISerialParity value) = 0;
+        /// @brief 校验位。
+        /// @return
+        virtual bsp::ISerialParity Parity() const = 0;
+        virtual void SetParity(bsp::ISerialParity value) = 0;
 
-		/// @brief 停止位个数。
-		/// @return
-		virtual bsp::ISerialStopBits StopBits() const = 0;
-		virtual void SetStopBits(bsp::ISerialStopBits value) = 0;
+        /// @brief 停止位个数。
+        /// @return
+        virtual bsp::ISerialStopBits StopBits() const = 0;
+        virtual void SetStopBits(bsp::ISerialStopBits value) = 0;
 
-		/// @brief 硬件流控。
-		/// @return
-		virtual bsp::ISerialHardwareFlowControl HardwareFlowControl() const = 0;
-		virtual void SetHardwareFlowControl(bsp::ISerialHardwareFlowControl value) = 0;
+        /// @brief 硬件流控。
+        /// @return
+        virtual bsp::ISerialHardwareFlowControl HardwareFlowControl() const = 0;
+        virtual void SetHardwareFlowControl(bsp::ISerialHardwareFlowControl value) = 0;
 
-		/// @brief 计算 frame_count 个帧占用多少个波特。
-		/// @param frame_count
-		/// @return
-		uint32_t CalculateFramesBaudCount(uint32_t frame_count);
-	};
-}
+        /// @brief 计算 frame_count 个帧占用多少个波特。
+        /// @param frame_count
+        /// @return
+        uint32_t CalculateFramesBaudCount(uint32_t frame_count) const;
+    };
+} // namespace bsp
