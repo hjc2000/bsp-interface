c2094fc1f0596bbae70b0a74ccbf792e8c35a2d0
hjc2000 <hjc.666@foxmail.com>
2024-08-28 14:53:39 +0800

    virtual void *Handle() = 0;

--- include/bsp-interface/dma/IDmaChannel.h
+++ include/bsp-interface/dma/IDmaChannel.h
@@ -10,5 +10,9 @@ namespace bsp
     public:
         virtual std::string Name() = 0;
         virtual void Open(bsp::IDmaOptions const &options) = 0;
+
+        /// @brief 返回底层的句柄。
+        /// @return
+        virtual void *Handle() = 0;
     };
 } // namespace bsp
