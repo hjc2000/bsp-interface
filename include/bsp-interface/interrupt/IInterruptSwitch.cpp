bfaa453a36eb52e26299bceae818c173e16a224e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-29 19:39:58 +0800

    // 保证即使抛出异常也能恢复全局中断的开启

--- include/bsp-interface/interrupt/IInterruptSwitch.cpp
+++ include/bsp-interface/interrupt/IInterruptSwitch.cpp
@@ -4,6 +4,8 @@
 void bsp::IInterruptSwitch::DoGlobalCriticalWork(std::function<void()> func)
 {
     DisableGlobalInterrupt();
+
+    // 保证即使抛出异常也能恢复全局中断的开启
     base::Guard g{
         [this]()
         {
