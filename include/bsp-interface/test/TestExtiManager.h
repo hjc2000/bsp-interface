d05726242a3147db66d868ea4f94be557417c068
hjc2000 <hjc.666@foxmail.com>
2024-08-29 15:36:44 +0800

    test

--- include/bsp-interface/test/TestExtiManager.h
+++ include/bsp-interface/test/TestExtiManager.h
@@ -1,20 +1,20 @@
 #pragma once
-#include <bsp-interface/di.h>
-#include <bsp-interface/interrupt/IExtiManager.h>
+#include <bsp-interface/di/interrupt.h>
+#include <bsp-interface/di/led.h>
 
 namespace bsp
 {
-	inline void TestExtiManager(int line_id)
-	{
-		DI_ExtiManager().Register(
-			line_id,
-			[]()
-			{
-				DI_RedDigitalLed().Toggle();
-			});
+    inline void TestExtiManager(int line_id)
+    {
+        DI_ExtiManager().Register(
+            line_id,
+            []()
+            {
+                DI_RedDigitalLed().Toggle();
+            });
 
-		while (true)
-		{
-		}
-	}
-}
+        while (true)
+        {
+        }
+    }
+} // namespace bsp
