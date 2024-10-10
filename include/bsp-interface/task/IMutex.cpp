54eaae141efb18a6db47616ae248fa925f9b4c31
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-10-10 22:53:41 +0800

    IMutex

---
+++ include/bsp-interface/task/IMutex.cpp
@@ -0,0 +1,1 @@
+#include "IMutex.h"
---
+++ include/bsp-interface/task/IMutex.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <base/LockGuard.h>
+
+namespace bsp
+{
+    class IMutex :
+        public base::ILock
+    {
+    public:
+    };
+} // namespace bsp
