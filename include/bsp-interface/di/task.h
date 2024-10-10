e9765dc64b3f087b0231600f7fe49bfd2758751b
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-10-10 22:55:39 +0800

    std::shared_ptr<bsp::IMutex> DICreate_Mutex();

--- include/bsp-interface/di/task.h
+++ include/bsp-interface/di/task.h
@@ -1,5 +1,11 @@
 #pragma once
+#include <bsp-interface/task/IMutex.h>
 #include <bsp-interface/task/ITaskManager.h>
+#include <memory>
+
+/// @brief 构造一个互斥量。
+/// @return
+std::shared_ptr<bsp::IMutex> DICreate_Mutex();
 
 /// @brief 获取任务管理器。
 /// @return
