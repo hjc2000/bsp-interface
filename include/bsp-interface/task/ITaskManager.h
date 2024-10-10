192296791b6b620aa4444d027aaf84a5d59543c3
hjc2000 <hjc.666@foxmail.com>
2024-10-10 17:23:05 +0800

    ITask

--- include/bsp-interface/task/ITaskManager.h
+++ include/bsp-interface/task/ITaskManager.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <bsp-interface/task/ITask.h>
 #include <cstdint>
 #include <functional>
+#include <memory>
 
 namespace bsp
 {
@@ -8,12 +10,12 @@ namespace bsp
     class ITaskManager
     {
     public:
-        /// @brief 启动运行一个任务
+        /// @brief 创建一个任务
         ///
         /// @param func 任务函数
         ///
         /// @param stack_size 任务栈大小。单位：字。
         /// @note 对于 32 位宽的 CPU ，一个字是 32 位，即 4 个字节。
-        virtual void RunTask(std::function<void()> func, uint32_t stack_size) = 0;
+        virtual std::shared_ptr<bsp::ITask> Create(std::function<void()> func, uint32_t stack_size) = 0;
     };
 } // namespace bsp
