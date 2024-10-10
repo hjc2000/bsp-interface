3941ef5bdbff1a0177c11c511189583adbd7ffae
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-10-10 23:38:53 +0800

    std::shared_ptr<bsp::IBinarySemaphore> DICreate_BinarySemaphore();

--- include/bsp-interface/di/task.h
+++ include/bsp-interface/di/task.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <bsp-interface/task/IBinarySemaphore.h>
 #include <bsp-interface/task/IMutex.h>
 #include <bsp-interface/task/ITaskManager.h>
 #include <memory>
@@ -6,6 +7,10 @@
 /// @brief 构造一个互斥量。
 /// @return
 std::shared_ptr<bsp::IMutex> DICreate_Mutex();
+
+/// @brief 构造一个二进制信号量。
+/// @return
+std::shared_ptr<bsp::IBinarySemaphore> DICreate_BinarySemaphore();
 
 /// @brief 获取任务管理器。
 /// @return
