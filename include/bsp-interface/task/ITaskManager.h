14e6d95aeced01eb89d0f64a950dffba70036d85
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-10-10 22:16:57 +0800

    /// @brief 启动调度。本函数会持续阻塞。         virtual void StartScheduler() = 0;

--- include/bsp-interface/task/ITaskManager.h
+++ include/bsp-interface/task/ITaskManager.h
@@ -17,5 +17,8 @@ namespace bsp
         /// @param stack_size 任务栈大小。单位：字。
         /// @note 对于 32 位宽的 CPU ，一个字是 32 位，即 4 个字节。
         virtual std::shared_ptr<bsp::ITask> Create(std::function<void()> func, uint32_t stack_size) = 0;
+
+        /// @brief 启动调度。本函数会持续阻塞。
+        virtual void StartScheduler() = 0;
     };
 } // namespace bsp
