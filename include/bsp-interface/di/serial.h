e6bbde1a137e0ce89deaa98d58d58da0682000d6
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-11-03 00:18:28 +0800

    注释

--- include/bsp-interface/di/serial.h
+++ include/bsp-interface/di/serial.h
@@ -3,6 +3,10 @@
 #include <bsp-interface/serial/ISerial.h>
 #include <memory>
 
+/// @brief 创建一个配置选项对象。
+/// @note 默认的选项为：1 位起始位，8 位数据位，1 位停止位，无校验，波特率 115200，
+/// 无硬件流控，数据双向传输。
+/// @return
 std::shared_ptr<bsp::ISerialOptions> DICreate_ISerialOptions();
 
 /// @brief 主串口。
