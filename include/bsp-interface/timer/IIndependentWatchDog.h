91fe9c289a02c1036eed281de199ee2bbe8e8bf4
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-11-03 21:32:10 +0800

    /// @note 有的单片机的独立看门狗一旦开启后就关不掉了，调用本函数不会起作用。

--- include/bsp-interface/timer/IIndependentWatchDog.h
+++ include/bsp-interface/timer/IIndependentWatchDog.h
@@ -12,6 +12,7 @@ namespace bsp
         virtual void Open(std::chrono::milliseconds timeout) = 0;
 
         /// @brief 关闭看门狗定时器。
+        /// @note 有的单片机的独立看门狗一旦开启后就关不掉了，调用本函数不会起作用。
         virtual void Close() = 0;
 
         /// @brief 看门狗超时时间。
