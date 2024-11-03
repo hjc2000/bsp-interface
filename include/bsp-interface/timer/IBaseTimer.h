22e0d15ba4ca96eb75aa0eb39ae74f702e318994
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-11-03 22:38:24 +0800

    SetElapsedHandle

--- include/bsp-interface/timer/IBaseTimer.h
+++ include/bsp-interface/timer/IBaseTimer.h
@@ -23,6 +23,6 @@ namespace bsp
         /// @warning 实现者需要将本函数实现为线程安全和可重入。最简单的方法就是在本函数的
         /// 操作期间禁用全局中断。
         /// @param func
-        virtual void SetElapsedCallback(std::function<void()> func) = 0;
+        virtual void SetElapsedHandle(std::function<void()> func) = 0;
     };
 } // namespace bsp
