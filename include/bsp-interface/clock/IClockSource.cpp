384ac59719e40a335eeffa7366e3c2bcd04fa30b
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-11-09 23:23:33 +0800

    IClockSource

--- include/bsp-interface/clock/IClockSource.cpp
+++ include/bsp-interface/clock/IClockSource.cpp
@@ -1,13 +1,1 @@
 #include "IClockSource.h"
-
-void bsp::IClockSource::TurnOn()
-{
-    TurnOn(1, 1);
-}
-
-void bsp::IClockSource::SetAsBypass(base::Hz external_clock_frequency)
-{
-    SetAsBypass(external_clock_frequency,
-                1,
-                1);
-}
--- include/bsp-interface/clock/IClockSource.h
+++ include/bsp-interface/clock/IClockSource.h
@@ -2,6 +2,7 @@
 #include <base/unit/Hz.h>
 #include <cstdint>
 #include <string>
+#include <vector>
 
 namespace bsp
 {
@@ -17,7 +18,6 @@ namespace bsp
     class IClockSource
     {
     public:
-#pragma region 接口
         /// @brief 本时钟源的名称。
         /// @return
         virtual std::string Name() const = 0;
@@ -26,28 +26,25 @@ namespace bsp
         /// @param crystal_frequency 有的时钟源需要外部晶振，且晶振允许的频率是一个范围，
         /// 此时需要告诉本对象该晶振的频率才能打开本时钟源。这种情况下必须使用本 TurnOn 重载，
         /// 否则会抛出异常。
-        /// @param input_division 输入分频系数。
-        /// @param output_division 输出分频系数。
+        /// @param target_frequencies 想让本时钟源输出的目标频率列表。因为有的时钟源可能有多个输出
+        /// 通道，所以就采用了列表的形式。
         virtual void TurnOn(base::Hz const &crystal_frequency,
-                            int32_t input_division,
-                            int32_t output_division) = 0;
+                            std::vector<base::Hz> const &target_frequencies) = 0;
 
         /// @brief 打开时钟源。
-        /// @param input_division 输入分频系数。
-        /// @param output_division 输出分频系数。
-        virtual void TurnOn(int32_t input_division,
-                            int32_t output_division) = 0;
+        /// @param target_frequencies 想让本时钟源输出的目标频率列表。因为有的时钟源可能有多个输出
+        /// 通道，所以就采用了列表的形式。
+        virtual void TurnOn(std::vector<base::Hz> const &target_frequencies) = 0;
 
         /// @brief 关闭时钟源。
         virtual void TurnOff() = 0;
 
         /// @brief 设置为旁路。
         /// @param external_clock_frequency 外部时钟频率。
-        /// @param input_division 输入分频系数。
-        /// @param output_division 输出分频系数。
+        /// @param target_frequencies 想让本时钟源输出的目标频率列表。因为有的时钟源可能有多个输出
+        /// 通道，所以就采用了列表的形式。
         virtual void SetAsBypass(base::Hz external_clock_frequency,
-                                 int32_t input_division,
-                                 int32_t output_division) = 0;
+                                 std::vector<base::Hz> const &target_frequencies) = 0;
 
         /// @brief 本时钟源当前的状态。
         /// @return
@@ -56,15 +53,5 @@ namespace bsp
         /// @brief 本时钟源当前频率。
         /// @return
         virtual base::Hz Frequency() const = 0;
-#pragma endregion
-
-        /// @brief 打开时钟源。
-        /// @note 输入分频和输出分频都设置为 1.
-        void TurnOn();
-
-        /// @brief 设置为旁路。
-        /// @note 输入分频和输出分频都设置为 1.
-        /// @param external_clock_frequency 外部时钟频率。
-        void SetAsBypass(base::Hz external_clock_frequency);
     };
 } // namespace bsp
