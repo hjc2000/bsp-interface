95bacc81f8ef32acd1256fd0a27834c009cdc594
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-11-10 00:34:26 +0800

    IClockSource

--- include/bsp-interface/clock/IClockSource.cpp
+++ include/bsp-interface/clock/IClockSource.cpp
@@ -1,1 +1,55 @@
 #include "IClockSource.h"
+
+#pragma region Open
+
+void bsp::IClockSource::Open()
+{
+    throw std::runtime_error{"不支持此重载版本。"};
+}
+
+void bsp::IClockSource::Open(base::Hz const &crystal_frequency)
+{
+    throw std::runtime_error{"不支持此重载版本。"};
+}
+
+void bsp::IClockSource::Open(std::string const &input_channel_name)
+{
+    throw std::runtime_error{"不支持此重载版本。"};
+}
+
+void bsp::IClockSource::Open(IClockSource_TargetOutputFrequency const &target_output_frequency)
+{
+    throw std::runtime_error{"不支持此重载版本。"};
+}
+
+void bsp::IClockSource::Open(base::IDictionary<std::string, base::Hz> const &target_output_frequencies)
+{
+    throw std::runtime_error{"不支持此重载版本。"};
+}
+
+void bsp::IClockSource::Open(std::string const &input_channel_name,
+                             base::IDictionary<std::string, base::Hz> const &target_output_frequencies)
+{
+    throw std::runtime_error{"不支持此重载版本。"};
+}
+
+void bsp::IClockSource::Open(std::string const &input_channel_name, base::Hz const &crystal_frequency, base::IDictionary<std::string, base::Hz> const &target_output_frequencies)
+{
+    throw std::runtime_error{"不支持此重载版本。"};
+}
+
+#pragma endregion
+
+#pragma region SetAsBypass
+
+void bsp::IClockSource::SetAsBypass(base::Hz external_clock_frequency)
+{
+    throw std::runtime_error{"不支持此重载版本。"};
+}
+
+void bsp::IClockSource::SetAsBypass(base::Hz external_clock_frequency, base::IDictionary<std::string, base::Hz> const &target_output_frequencies)
+{
+    throw std::runtime_error{"不支持此重载版本。"};
+}
+
+#pragma endregion
--- include/bsp-interface/clock/IClockSource.h
+++ include/bsp-interface/clock/IClockSource.h
@@ -2,6 +2,7 @@
 #include <base/container/IDictionary.h>
 #include <base/unit/Hz.h>
 #include <cstdint>
+#include <stdexcept>
 #include <string>
 
 namespace bsp
@@ -14,6 +15,32 @@ namespace bsp
         Bypass,
     };
 
+#pragma region 参数类
+
+    class IClockSource_TargetOutputFrequency
+    {
+    private:
+        base::Hz _value{};
+
+    public:
+        explicit IClockSource_TargetOutputFrequency(base::Hz const &value)
+        {
+            _value = value;
+        }
+
+        base::Hz Value() const
+        {
+            return _value;
+        }
+
+        void SetValue(base::Hz const &value)
+        {
+            _value = value;
+        }
+    };
+
+#pragma endregion
+
     /// @brief 时钟源接口
     class IClockSource
     {
@@ -21,6 +48,39 @@ namespace bsp
         /// @brief 本时钟源的名称。
         /// @return
         virtual std::string Name() const = 0;
+
+#pragma region Open
+        /// @brief 打开时钟源。
+        /// @note 有的时钟源使用内部振荡器，不需要外部晶振，振荡器频率已知且固定，无法自定义，
+        /// 直接打开就行。本重载用于这种场景。
+        virtual void Open();
+
+        /// @brief 打开时钟源。
+        /// @note 有的时钟源非常简单，外部振荡器输入后，直接就输出同频率的时钟信号，
+        /// 不支持分频和倍频。本重载用于这种简单的时钟源。
+        /// @param crystal_frequency
+        virtual void Open(base::Hz const &crystal_frequency);
+
+        /// @brief 打开时钟源。
+        /// @param input_channel_name
+        virtual void Open(std::string const &input_channel_name);
+
+        /// @brief 打开时钟源。
+        /// @param target_output_frequency 目标输出频率
+        virtual void Open(IClockSource_TargetOutputFrequency const &target_output_frequency);
+
+        /// @brief 打开时钟源。
+        /// @param target_output_frequencies 想让本时钟源输出的目标频率列表。因为有的时钟源可能有多个输出
+        /// 通道，所以就采用了列表的形式。
+        virtual void Open(base::IDictionary<std::string, base::Hz> const &target_output_frequencies);
+
+        /// @brief 打开时钟源。
+        /// @param input_channel_name 输入通道名。因为有的时钟源可能有多个输入通道，所以使用字符串指定
+        /// 输入通道名。
+        /// @param target_output_frequencies 想让本时钟源输出的目标频率列表。因为有的时钟源可能有多个输出
+        /// 通道，所以就采用了列表的形式。
+        virtual void Open(std::string const &input_channel_name,
+                          base::IDictionary<std::string, base::Hz> const &target_output_frequencies);
 
         /// @brief 打开时钟源。
         /// @param input_channel_name 输入通道名。因为有的时钟源可能有多个输入通道，所以使用字符串指定
@@ -32,25 +92,24 @@ namespace bsp
         /// 输出通道，所以就采用字典来指定每个通道，key 为输出通道名，value 为输出通道目标频率。
         virtual void Open(std::string const &input_channel_name,
                           base::Hz const &crystal_frequency,
-                          base::IDictionary<std::string, base::Hz> const &target_output_frequencies) = 0;
+                          base::IDictionary<std::string, base::Hz> const &target_output_frequencies);
+#pragma endregion
 
-        /// @brief 打开时钟源。
-        /// @param input_channel_name 输入通道名。因为有的时钟源可能有多个输入通道，所以使用字符串指定
-        /// 输入通道名。
-        /// @param target_output_frequencies 想让本时钟源输出的目标频率列表。因为有的时钟源可能有多个输出
-        /// 通道，所以就采用了列表的形式。
-        virtual void Open(std::string const &input_channel_name,
-                          base::IDictionary<std::string, base::Hz> const &target_output_frequencies) = 0;
-
-        /// @brief 关闭时钟源。
-        virtual void Close() = 0;
+#pragma region SetAsBypass
+        /// @brief 设置为旁路。
+        /// @param external_clock_frequency
+        virtual void SetAsBypass(base::Hz external_clock_frequency);
 
         /// @brief 设置为旁路。
         /// @param external_clock_frequency 外部时钟频率。
         /// @param target_output_frequencies 想让本时钟源输出的目标频率列表。因为有的时钟源可能有多个输出
         /// 通道，所以就采用了列表的形式。
         virtual void SetAsBypass(base::Hz external_clock_frequency,
-                                 base::IDictionary<std::string, base::Hz> const &target_output_frequencies) = 0;
+                                 base::IDictionary<std::string, base::Hz> const &target_output_frequencies);
+#pragma endregion
+
+        /// @brief 关闭时钟源。
+        virtual void Close() = 0;
 
         /// @brief 本时钟源当前的状态。
         /// @return
