04e5ed7bb731f8249fae2b2d4278ae39a4d6c05b
hjc2000 <hjc.666@foxmail.com>
2024-12-03 11:47:53 +0800

    IClockSignal

--- include/bsp-interface/clock/IClockSignal.h
+++ include/bsp-interface/clock/IClockSignal.h
@@ -14,5 +14,15 @@ namespace bsp
         /// @brief 时钟信号的频率
         /// @return
         virtual base::Hz Frequency() const = 0;
+
+        /// @brief 设置输入分频系数。
+        /// @note 有些时钟信号不支持。
+        /// @param value
+        virtual void SetInputDivisionFactor(uint32_t value) = 0;
+
+        /// @brief 设置输出分频系数。
+        /// @note 有些时钟信号不支持。
+        /// @param value
+        virtual void SetOutputDivisionFactor(uint32_t value) = 0;
     };
 } // namespace bsp
