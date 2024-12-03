1c778a9feb94612731a4b98e37d686a46678b798
hjc2000 <hjc.666@foxmail.com>
2024-12-03 10:42:56 +0800

    DI_GetClockSignalFrequency

--- include/bsp-interface/di/clock.h
+++ include/bsp-interface/di/clock.h
@@ -1,8 +1,14 @@
 #pragma once
 #include <base/container/IDictionary.h>
+#include <base/unit/Hz.h>
 #include <bsp-interface/clock/IClockSource.h>
 #include <string>
 
 /// @brief 时钟源集合。
 /// @return
 base::IDictionary<std::string, bsp::IClockSource *> const &DI_ClockSourceCollection();
+
+/// @brief 查询指定的时钟信号的频率。
+/// @param clock_signal_name
+/// @return
+base::Hz DI_GetClockSignalFrequency(std::string const &clock_signal_name);
