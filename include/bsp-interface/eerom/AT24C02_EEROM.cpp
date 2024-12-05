6812e85baf1fcff2379c0faabc6284ee5e2e9f2f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-06 00:39:24 +0800

    AT24C02_EEROM

---
+++ include/bsp-interface/eerom/AT24C02_EEROM.cpp
@@ -0,0 +1,53 @@
+#include "AT24C02_EEROM.h"
+#include <bsp-interface/di/delayer.h>
+
+bsp::AT24C02_EEROM::AT24C02_EEROM(bsp::IIicHost *host)
+{
+    _iic_host = host;
+    _iic_host->Open();
+}
+
+int32_t bsp::AT24C02_EEROM::Size() const
+{
+    return 256;
+}
+
+uint8_t bsp::AT24C02_EEROM::ReadByte(int32_t addr)
+{
+    _iic_host->SendStartingSignal();
+    _iic_host->SendByte(0XA0 + ((addr / 256) << 1)); // 发送器件地址0XA0,写数据
+    _iic_host->SendByte(addr % 256);                 // 发送低地址
+    _iic_host->SendStartingSignal();
+    _iic_host->SendByte(0XA1); // 进入接收模式
+    uint8_t data = _iic_host->ReceiveByte(1);
+    _iic_host->SendStoppingSignal();
+    return data;
+}
+
+void bsp::AT24C02_EEROM::Read(int32_t addr, uint8_t *buffer, int32_t offset, int32_t count)
+{
+    Read(addr, base::Span{buffer + offset, count});
+}
+
+void bsp::AT24C02_EEROM::Read(int32_t addr, base::Span const &span)
+{
+    if (addr + span.Size() > Size())
+    {
+        throw std::out_of_range{"要从 EEROM 读取的数据超出储存器范围。"};
+    }
+
+    for (int i = 0; i < span.Size(); i++)
+    {
+        span[i] = ReadByte(addr + i);
+    }
+}
+
+void bsp::AT24C02_EEROM::WriteByte(int32_t addr, uint8_t data)
+{
+    _iic_host->SendStartingSignal();
+    _iic_host->SendByte(0XA0 + ((addr / 256) << 1)); // 发送器件地址0XA0,写数据
+    _iic_host->SendByte(addr % 256);                 // 发送低地址
+    _iic_host->SendByte(data);                       // 发送字节
+    _iic_host->SendStoppingSignal();                 // 产生一个停止条件
+    DI_Delayer().Delay(std::chrono::milliseconds{10});
+}
---
+++ include/bsp-interface/eerom/AT24C02_EEROM.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <base/stream/ReadOnlySpan.h>
+#include <bsp-interface/serial/IIicHost.h>
+#include <stdint.h>
+
+namespace bsp
+{
+    /// @brief AT24C02 EEROM 芯片驱动。
+    /// @note 这是一个使用 IIC 接口的 EEROM 芯片。
+    class AT24C02_EEROM
+    {
+    private:
+        bsp::IIicHost *_iic_host = nullptr;
+
+    public:
+        /// @brief 构造函数。
+        /// @param host 连接着本芯片的 IIC 主机接口对象。
+        AT24C02_EEROM(bsp::IIicHost *host);
+
+        /// @brief EEROM 的容量。单位：字节。
+        /// @return
+        int32_t Size() const;
+
+        /// @brief 读取 1 个字节的数据。
+        /// @param addr 数据地址。
+        /// @return 读取到的字节。
+        uint8_t ReadByte(int32_t addr);
+
+        void Read(int32_t addr, uint8_t *buffer, int32_t offset, int32_t count);
+
+        void Read(int32_t addr, base::Span const &span);
+
+        /// @brief 写 1 个字节的数据。
+        /// @param addr 数据地址。
+        /// @param data 数据。
+        void WriteByte(int32_t addr, uint8_t data);
+    };
+} // namespace bsp
