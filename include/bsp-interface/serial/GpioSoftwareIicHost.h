1c802cfbee46f17f4e873362f1191bbafed021bb
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-05 23:32:05 +0800

    GpioSoftwareIicHost

--- include/bsp-interface/serial/GpioSoftwareIicHost.h
+++ include/bsp-interface/serial/GpioSoftwareIicHost.h
@@ -13,8 +13,12 @@ namespace bsp
         std::string _scl_pin_name;
         std::string _sda_pin_name;
 
+        /// @brief 用来将引脚配置为输入模式的选项。
         std::shared_ptr<bsp::IGpioPinOptions> _input_pin_options;
+
+        /// @brief 用来将引脚配置为输出模式的选项。
         std::shared_ptr<bsp::IGpioPinOptions> _output_pin_options;
+
         bsp::IGpioPin *_scl_pin = nullptr;
         bsp::IGpioPin *_sda_pin = nullptr;
 
