15e1763068c525eacfdc67135c981c75ab153014
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-06 20:44:09 +0800

    删除_write函数

--- private_src/Console.cpp
+++ private_src/Console.cpp
@@ -37,31 +37,3 @@ void bsp::Console::SetOutStream(std::shared_ptr<base::Stream> value)
 {
     _out_stream = value;
 }
-
-#include <bsp-interface/di/console.h>
-#include <errno.h>
-#include <stdio.h>
-
-extern "C"
-{
-    /// @brief stdio 底层会调用本函数。
-    ///
-    /// @param file 文件描述符
-    /// @note 对于标准输出，file = 1，对于错误输出，file = 2。
-    ///
-    /// @param ptr
-    /// @param len
-    /// @return
-    int _write(int file, char *ptr, int len)
-    {
-        try
-        {
-            DI_Console().Write(ptr, len);
-            return len;
-        }
-        catch (...)
-        {
-            return EIO;
-        }
-    }
-}
