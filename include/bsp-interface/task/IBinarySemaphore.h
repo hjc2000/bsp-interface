4ecde599a6c479e3b6dc97763e9ab6302dcf7c1c
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-11 23:24:53 +0800

    IBinarySemaphore

--- include/bsp-interface/task/IBinarySemaphore.h
+++ include/bsp-interface/task/IBinarySemaphore.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <base/unit/Seconds.h>
 
 namespace bsp
 {
@@ -6,8 +7,19 @@ namespace bsp
     class IBinarySemaphore
     {
     public:
+        /// @brief 释放信号量。
         virtual void Release() = 0;
+
+        /// @brief 在中断中释放信号量。
         virtual void ReleaseFromISR() = 0;
+
+        /// @brief 获取信号量。无限等待，永不超时。
         virtual void Acquire() = 0;
+
+        /// @brief 尝试获取信号量。超时后会失败，并返回 false，成功获取信号量后将
+        /// 返回 true.
+        /// @param timeout 超时时间。
+        /// @return
+        virtual bool TryAcquire(base::Seconds const &timeout) = 0;
     };
 } // namespace bsp
