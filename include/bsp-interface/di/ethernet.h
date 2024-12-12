d8bfd7372b1e8ef63b40dc63655f811e46a6846e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-12 22:57:03 +0800

    DI_EthernetPort

--- include/bsp-interface/di/ethernet.h
+++ include/bsp-interface/di/ethernet.h
@@ -1,6 +1,11 @@
 #pragma once
 #include <bsp-interface/ethernet/IEthernetController.h>
+#include <bsp-interface/ethernet/IEthernetPort.h>
 
 /// @brief 以太网控制器。
 /// @return
 bsp::IEthernetController &DI_EthernetController();
+
+/// @brief 以太网端口。
+/// @return
+bsp::IEthernetPort &DI_EthernetPort();
