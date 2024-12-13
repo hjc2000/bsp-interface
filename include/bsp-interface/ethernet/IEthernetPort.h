7a31aa3b7153d39804f8c8ec69bdc4eba9e1870d
hjc2000 <hjc.666@foxmail.com>
2024-12-13 10:55:14 +0800

    IEthernetPort

--- include/bsp-interface/ethernet/IEthernetPort.h
+++ include/bsp-interface/ethernet/IEthernetPort.h
@@ -4,35 +4,41 @@
 
 namespace bsp
 {
-    /// @brief 以太网端口。
-    /// @note 端口就是以太网控制器 + PHY 芯片。
-    /// @note 鉴于 PHY 芯片各品牌，各型号都不一样，各个 PHY 芯片都要定义一个类，继承本接口，
-    /// 基于 bsp::IEthernetController 对象来实现以太网端口的功能。
-    class IEthernetPort
-    {
-    public:
-        /// @brief 以太网控制器的名称。
-        /// @return
-        virtual std::string Name() const = 0;
+	/// @brief 以太网端口。
+	/// @note 端口就是以太网控制器 + PHY 芯片。
+	/// @note 鉴于 PHY 芯片各品牌，各型号都不一样，各个 PHY 芯片都要定义一个类，继承本接口，
+	/// 基于 bsp::IEthernetController 对象来实现以太网端口的功能。
+	class IEthernetPort
+	{
+	public:
+		/// @brief 以太网控制器的名称。
+		/// @return
+		virtual std::string Name() const = 0;
 
-        /// @brief 打开以太网端口。
-        /// @param mac MAC 地址。
-        virtual void Open(base::Mac const &mac) = 0;
+		/// @brief 打开以太网端口。
+		/// @param mac MAC 地址。
+		virtual void Open(base::Mac const &mac) = 0;
 
-        /// @brief 读 PHY 的寄存器
-        /// @param register_index 寄存器索引。
-        /// @return
-        virtual uint32_t ReadPHYRegister(uint32_t register_index) = 0;
+		/// @brief 读 PHY 的寄存器
+		/// @param register_index 寄存器索引。
+		/// @return
+		virtual uint32_t ReadPHYRegister(uint32_t register_index) = 0;
 
-        /// @brief 写 PHY 的寄存器。
-        /// @param register_index 寄存器索引。
-        /// @param value
-        virtual void WritePHYRegister(uint32_t register_index, uint32_t value) = 0;
+		/// @brief 写 PHY 的寄存器。
+		/// @param register_index 寄存器索引。
+		/// @param value
+		virtual void WritePHYRegister(uint32_t register_index, uint32_t value) = 0;
 
-        /// @brief 复位 PHY 芯片。
-        virtual void ResetPHY() = 0;
+		/// @brief 复位 PHY 芯片。
+		virtual void ResetPHY() = 0;
 
-        /// @brief 使能自动协商。
-        virtual void EnableAutoNegotiation() = 0;
-    };
+		/// @brief 使能自动协商。
+		virtual void EnableAutoNegotiation() = 0;
+
+		virtual void EnablePowerDownMode() = 0;
+		virtual void DisablePowerDownMode() = 0;
+
+		virtual void EnableLoopbackMode() = 0;
+		virtual void DisableLoopbackMode() = 0;
+	};
 } // namespace bsp
