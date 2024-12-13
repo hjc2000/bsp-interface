9db2ec501d9dc0935dd8fee241aad028f423b5c3
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-14 00:06:02 +0800

    LAN8720A_EthernetPort

--- include/bsp-interface/ethernet/phy/LAN8720A_EthernetPort.h
+++ include/bsp-interface/ethernet/phy/LAN8720A_EthernetPort.h
@@ -3,6 +3,11 @@
 
 namespace bsp
 {
+	/// @brief 为 LAN8720A PHY 芯片部分实现 bsp::IEthernetPort.
+	/// @note 无法完整实现 bsp::IEthernetPort，因为有些东西是板级的，例如 LAN8720A
+	/// 需要一个引脚去硬件复位，而连接哪个 GPIO 引脚，或者连接到哪个扩展 IO 端子上，这些
+	/// 都是具体的电路板去实现的。这部分实现要交给板级开发。板级开发继承本类，继续实现
+	/// bsp::IEthernetPort 接口。
 	class LAN8720A_EthernetPort :
 		public bsp::IEthernetPort
 	{
