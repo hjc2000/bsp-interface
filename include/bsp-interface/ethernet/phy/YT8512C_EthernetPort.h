e02840fd03d16a33b30ca031f71eaf27765797a2
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-15 15:18:19 +0800

    YT8512C

--- include/bsp-interface/ethernet/phy/YT8512C_EthernetPort.h
+++ include/bsp-interface/ethernet/phy/YT8512C_EthernetPort.h
@@ -3,8 +3,8 @@
 
 namespace bsp
 {
-	/// @brief 为 LAN8720A PHY 芯片部分实现 bsp::IEthernetPort.
-	/// @note 无法完整实现 bsp::IEthernetPort，因为有些东西是板级的，例如 LAN8720A
+	/// @brief 为 YT8512C PHY 芯片部分实现 bsp::IEthernetPort.
+	/// @note 无法完整实现 bsp::IEthernetPort，因为有些东西是板级的，例如 YT8512C
 	/// 需要一个引脚去硬件复位，而连接哪个 GPIO 引脚，或者连接到哪个扩展 IO 端子上，这些
 	/// 都是具体的电路板去实现的。这部分实现要交给板级开发。板级开发继承本类，继续实现
 	/// bsp::IEthernetPort 接口。
