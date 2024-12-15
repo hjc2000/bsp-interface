6426fa7cdf4be9846e85edc20f1ef57e6844d250
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-15 18:40:54 +0800

    YT8512C_EthernetPort

--- include/bsp-interface/ethernet/phy/YT8512C_EthernetPort.cpp
+++ include/bsp-interface/ethernet/phy/YT8512C_EthernetPort.cpp
@@ -58,9 +58,11 @@ void bsp::YT8512C_EthernetPort::DisableLoopbackMode()
 
 bsp::Ethernet_DuplexMode bsp::YT8512C_EthernetPort::DuplexMode()
 {
-	uint32_t register_value = ReadPHYRegister(0x1F);
-	uint32_t const mask = 0b10000;
-	if (register_value & mask)
+	uint32_t register_value = ReadPHYRegister(0x11);
+	uint32_t const mask = 0b1 << 13;
+	uint32_t duplex_register = (register_value & mask) >> 13;
+
+	if (duplex_register)
 	{
 		DI_Console().WriteLine("全双工");
 		return bsp::Ethernet_DuplexMode::FullDuplex;
@@ -72,14 +74,27 @@ bsp::Ethernet_DuplexMode bsp::YT8512C_EthernetPort::DuplexMode()
 
 base::Bps bsp::YT8512C_EthernetPort::Speed()
 {
-	uint32_t register_value = ReadPHYRegister(0x1F);
-	uint32_t const mask = 0b01000;
-	if (register_value & mask)
+	uint32_t register_value = ReadPHYRegister(0x11);
+	uint32_t const mask = 0b11 << 14;
+	uint32_t duplex_register = (register_value & mask) >> 14;
+
+	if (duplex_register == 0b00)
+	{
+		DI_Console().WriteLine("连接速率：10 Mbps");
+		return base::Mbps{10};
+	}
+	else if (duplex_register == 0b01)
 	{
 		DI_Console().WriteLine("连接速率：100 Mbps");
 		return base::Mbps{100};
 	}
-
-	DI_Console().WriteLine("连接速率：10 Mbps");
-	return base::Mbps{10};
+	else if (duplex_register == 0b01)
+	{
+		DI_Console().WriteLine("连接速率：1000 Mbps");
+		return base::Mbps{1000};
+	}
+	else
+	{
+		throw std::runtime_error{"读取连接速率寄存器时读取到保留值。"};
+	}
 }
