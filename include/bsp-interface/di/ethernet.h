bc1eb30488cf8f6e72e6881b77917d7de4ed915d
hjc2000 <hjc.666@foxmail.com>
2024-12-18 16:52:23 +0800

    注释

--- include/bsp-interface/di/ethernet.h
+++ include/bsp-interface/di/ethernet.h
@@ -3,9 +3,12 @@
 #include <bsp-interface/ethernet/IEthernetPort.h>
 
 /// @brief 以太网控制器。
+/// @note 负责单片机外设的人实现此函数。
 /// @return
 bsp::IEthernetController &DI_EthernetController();
 
 /// @brief 以太网端口。
+/// @note 负责 PHY 的人，利用 DI_EthernetController 抽象出来的单片机的以太网控制器来操作 PHY，
+/// 实现本函数。
 /// @return
 bsp::IEthernetPort &DI_EthernetPort();
