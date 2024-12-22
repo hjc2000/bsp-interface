c27ff07f86e736b17b996a63d17c2442f4345855
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-22 13:35:43 +0800

    DI_EnableCache

---
+++ include/bsp-interface/di/cache.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void DI_EnableCache();
+
+void DI_DisableCache();
--- include/bsp-interface/di/reset_initialize.h
+++ include/bsp-interface/di/reset_initialize.h
@@ -3,6 +3,12 @@
 /// @brief 复位单片机。
 void DI_Reset();
 
+/// @brief 初始化 CPU.
+/// @note 本函数需要被 DI_Initialize 调用。别的地方都不要调用。
+/// @note 这里用来执行一些非常底层的 CPU 的初始化，甚至比时钟配置还要底层。
+/// 例如使能 FPU，使能 cache，开启第二个核心，......等。
+void DI_InitializeCPU();
+
 /// @brief 执行初始化工作。
 /// @note 此函数需要实现为幂等。
 /// @note 这个函数需要在 main 函数一开始调用。
