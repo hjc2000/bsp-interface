02e1ed5c2993bd319d00e410f2c7e218c3bed3b4
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-22 13:55:51 +0800

    cache

--- include/bsp-interface/di/cache.h
+++ include/bsp-interface/di/cache.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <stddef.h>
+#include <stdint.h>
 
 /// @brief 使能缓存。
 void DI_EnableCache();
@@ -9,6 +11,12 @@ void DI_DisableCache();
 /// @brief 是 D-Cache 无效。
 /// @note 即将 D-Cache 的数据标记为过期的，重新从内存中读取数据放到 D-Cache 中。
 void DI_InvalidateDCache();
+
+/// @brief 将指定内存地址处的指定大小的数据进行 D-Cache 无效化。即如果这个地方的数据
+/// 有被 D-Cache 缓存，则将对应的缓存无效化。
+/// @param address
+/// @param size
+void DI_InvalidateDCache(void *address, int32_t size);
 
 /// @brief 清除 D-Cache。
 /// @note 即将原本从内存中读取到 D-Cache 中，并且已经修改过的 D-Cache 的数据写入
