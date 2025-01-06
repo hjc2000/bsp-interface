f4c4adc3502f373ac84f84201c692c7f843ecc78
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-07 00:39:42 +0800

    IPhyDriver

--- include/bsp-interface/ethernet/phy/IPhyDriver.h
+++ include/bsp-interface/ethernet/phy/IPhyDriver.h
@@ -20,7 +20,7 @@ namespace bsp
 		virtual base::Bps Speed() = 0;
 
 		/// @brief 软件复位 PHY.
-		virtual void SoftwareReset();
+		void SoftwareReset();
 
 		/// @brief 通过 PHY 的复位引脚将 PHY 复位。
 		void HardwareReset()
@@ -31,27 +31,27 @@ namespace bsp
 #pragma region 自动协商
 		/// @brief 检查本网口是否支持自动协商。
 		/// @return 支持则返回 true，不支持则返回 false.
-		virtual bool SupportAutoNegotiation();
+		bool SupportAutoNegotiation();
 
 		/// @brief 使能自动协商。
 		/// @note 会先判断 PHY 是否支持自动协商，不支持自动协商会抛出异常。
 		/// @note 使能自动协商后会等待 PHY 自动协商完成。等待超时后会抛出异常。
-		virtual void EnableAutoNegotiation();
+		void EnableAutoNegotiation();
 
 		/// @brief 自动协商完成。
 		/// @return
-		virtual bool AutoNegotiationCompleted();
+		bool AutoNegotiationCompleted();
 #pragma endregion
 
-		virtual void EnablePowerDownMode();
-		virtual void DisablePowerDownMode();
+		void EnablePowerDownMode();
+		void DisablePowerDownMode();
 
-		virtual void EnableLoopbackMode();
-		virtual void DisableLoopbackMode();
+		void EnableLoopbackMode();
+		void DisableLoopbackMode();
 
 		/// @brief 网线当前处于链接状态。
 		/// @return
-		virtual bool IsLinked();
+		bool IsLinked();
 
 		/// @brief 读 PHY 的寄存器
 		/// @param register_index 寄存器索引。
