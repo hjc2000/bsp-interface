214f4c5119cf5528c63e4fb739c97e0d7effee1c
hjc2000 <hjc.666@foxmail.com>
2025-01-10 15:24:38 +0800

    class IHeap

--- include/bsp-interface/IHeap.h
+++ include/bsp-interface/IHeap.h
@@ -4,10 +4,27 @@
 
 namespace bsp
 {
+	/// @brief 堆接口。
 	class IHeap
 	{
 	public:
 		virtual void *Malloc(size_t size) = 0;
 		virtual void Free(void *p) = 0;
+
+		virtual uint8_t *begin() = 0;
+		virtual uint8_t *end() = 0;
+
+		/// @brief 堆的总大小。
+		/// @return
+		size_t TotalSize() const
+		{
+			IHeap *self = const_cast<IHeap *>(this);
+			return self->end() - self->begin();
+		}
+
+		/// @brief 剩余的自由空间。单位：字节。
+		/// @note 不一定是一整块连续的，有可能是碎片化的。
+		/// @return
+		virtual size_t RemainingFreeSize() const = 0;
 	};
 } // namespace bsp
