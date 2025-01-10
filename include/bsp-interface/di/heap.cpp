22566433face423d8e3660f31858ddd3054bceb9
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-10 23:22:39 +0800

    void *operator new(size_t size)

--- include/bsp-interface/di/heap.cpp
+++ include/bsp-interface/di/heap.cpp
@@ -1,1 +1,84 @@
 #include "heap.h"
+#include <bsp-interface/di/interrupt.h>
+
+void *operator new(size_t size)
+{
+	bsp::GlobalInterruptGuard g;
+	void *ptr = DI_Heap().Malloc(size);
+	if (ptr == nullptr)
+	{
+		throw std::bad_alloc{};
+	}
+
+	return ptr;
+}
+
+void *operator new[](size_t size)
+{
+	bsp::GlobalInterruptGuard g;
+	void *ptr = DI_Heap().Malloc(size);
+	if (ptr == nullptr)
+	{
+		throw std::bad_alloc{};
+	}
+
+	return ptr;
+}
+
+void *operator new(size_t size, std::nothrow_t const &) noexcept
+{
+	bsp::GlobalInterruptGuard g;
+	void *p = DI_Heap().Malloc(size);
+	if (p == nullptr)
+	{
+	}
+
+	return p;
+}
+
+void *operator new[](size_t size, std::nothrow_t const &) noexcept
+{
+	bsp::GlobalInterruptGuard g;
+	void *p = DI_Heap().Malloc(size);
+	if (p == nullptr)
+	{
+	}
+
+	return p;
+}
+
+void operator delete(void *ptr) noexcept
+{
+	bsp::GlobalInterruptGuard g;
+	DI_Heap().Free(ptr);
+}
+
+void operator delete[](void *ptr) noexcept
+{
+	bsp::GlobalInterruptGuard g;
+	DI_Heap().Free(ptr);
+}
+
+void operator delete(void *ptr, std::nothrow_t const &) noexcept
+{
+	bsp::GlobalInterruptGuard g;
+	DI_Heap().Free(ptr);
+}
+
+void operator delete[](void *ptr, std::nothrow_t const &) noexcept
+{
+	bsp::GlobalInterruptGuard g;
+	DI_Heap().Free(ptr);
+}
+
+void operator delete(void *ptr, size_t size) noexcept
+{
+	bsp::GlobalInterruptGuard g;
+	DI_Heap().Free(ptr);
+}
+
+void operator delete[](void *ptr, size_t size) noexcept
+{
+	bsp::GlobalInterruptGuard g;
+	DI_Heap().Free(ptr);
+}
