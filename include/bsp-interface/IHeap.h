3463951f1050fea2c4d06e3380c661114f10fb45
hjc2000 <hjc.666@foxmail.com>
2025-01-10 15:33:40 +0800

    class IHeap

--- include/bsp-interface/IHeap.h
+++ include/bsp-interface/IHeap.h
@@ -8,7 +8,13 @@ namespace bsp
 	class IHeap
 	{
 	public:
+		/// @brief 分配内存。
+		/// @param size 要分配的内存块大小。单位：字节。
+		/// @return
 		virtual void *Malloc(size_t size) = 0;
+
+		/// @brief 要释放的由 Malloc 方法分配的内存块。
+		/// @param p Malloc 方法返回的指针。
 		virtual void Free(void *p) = 0;
 
 		/// @brief 堆的起点。
@@ -19,6 +25,11 @@ namespace bsp
 		/// @return
 		virtual uint8_t *end() = 0;
 
+		/// @brief 剩余的自由空间。单位：字节。
+		/// @note 不一定是一整块连续的，有可能是碎片化的。
+		/// @return
+		virtual size_t RemainingFreeSize() const = 0;
+
 		/// @brief 堆的总大小。
 		/// @return
 		size_t TotalSize() const
@@ -26,10 +37,5 @@ namespace bsp
 			IHeap *self = const_cast<IHeap *>(this);
 			return self->end() - self->begin();
 		}
-
-		/// @brief 剩余的自由空间。单位：字节。
-		/// @note 不一定是一整块连续的，有可能是碎片化的。
-		/// @return
-		virtual size_t RemainingFreeSize() const = 0;
 	};
 } // namespace bsp
