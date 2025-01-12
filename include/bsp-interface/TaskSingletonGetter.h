b799674a4310831b13fcc762dcbe318de69d088c
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-13 00:40:33 +0800

    TaskSingletonGetter

--- include/bsp-interface/TaskSingletonGetter.h
+++ include/bsp-interface/TaskSingletonGetter.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <base/di/SingletonGetter.h>
+#include <base/task/IMutex.h>
 #include <bsp-interface/di/task.h>
 
 namespace bsp
@@ -10,15 +11,26 @@ namespace bsp
 	class TaskSingletonGetter :
 		public base::SingletonGetter<T>
 	{
-	public:
+	private:
+		inline static_field std::shared_ptr<base::IMutex> _lock = nullptr;
+
+	protected:
 		virtual void Lock() override
 		{
-			DI_SuspendAllTask();
+			{
+				bsp::TaskGuard g;
+				if (_lock == nullptr)
+				{
+					_lock = base::di::CreateMutex();
+				}
+			}
+
+			_lock->Lock();
 		}
 
 		virtual void Unlock() override
 		{
-			DI_ResumeAllTask();
+			_lock->Unlock();
 		}
 	};
 } // namespace bsp
