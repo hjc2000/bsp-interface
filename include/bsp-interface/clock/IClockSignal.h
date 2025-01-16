172409af6975043d21cee865d037647e167878a2
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-16 18:56:41 +0800

    MHz

--- include/bsp-interface/clock/IClockSignal.h
+++ include/bsp-interface/clock/IClockSignal.h
@@ -1,97 +1,97 @@
 #pragma once
-#include <base/unit/Hz.h>
+#include <base/unit/MHz.h>
 
 namespace bsp
 {
 #pragma region 参数类
 
-    class IClockSignal_InputDivisionFactor
-    {
-    private:
-        uint32_t _value = 0;
+	class IClockSignal_InputDivisionFactor
+	{
+	private:
+		uint32_t _value = 0;
 
-    public:
-        explicit IClockSignal_InputDivisionFactor(uint32_t value)
-        {
-            _value = value;
-        }
+	public:
+		explicit IClockSignal_InputDivisionFactor(uint32_t value)
+		{
+			_value = value;
+		}
 
-        uint32_t Value() const
-        {
-            return _value;
-        }
-    };
+		uint32_t Value() const
+		{
+			return _value;
+		}
+	};
 
-    class IClockSignal_OutputDivisionFactor
-    {
-    private:
-        uint32_t _value = 0;
+	class IClockSignal_OutputDivisionFactor
+	{
+	private:
+		uint32_t _value = 0;
 
-    public:
-        explicit IClockSignal_OutputDivisionFactor(uint32_t value)
-        {
-            _value = value;
-        }
+	public:
+		explicit IClockSignal_OutputDivisionFactor(uint32_t value)
+		{
+			_value = value;
+		}
 
-        uint32_t Value() const
-        {
-            return _value;
-        }
-    };
+		uint32_t Value() const
+		{
+			return _value;
+		}
+	};
 
-    class IClockSignal_ClockSource
-    {
-    private:
-        std::string _value;
+	class IClockSignal_ClockSource
+	{
+	private:
+		std::string _value;
 
-    public:
-        explicit IClockSignal_ClockSource(std::string const &value)
-        {
-            _value = value;
-        }
+	public:
+		explicit IClockSignal_ClockSource(std::string const &value)
+		{
+			_value = value;
+		}
 
-        std::string Value() const
-        {
-            return _value;
-        }
-    };
+		std::string Value() const
+		{
+			return _value;
+		}
+	};
 
 #pragma endregion
 
-    /// @brief 时钟信号
-    class IClockSignal
-    {
-    public:
-        /// @brief 时钟信号的名称。
-        /// @return
-        virtual std::string Name() const = 0;
+	/// @brief 时钟信号
+	class IClockSignal
+	{
+	public:
+		/// @brief 时钟信号的名称。
+		/// @return
+		virtual std::string Name() const = 0;
 
-        /// @brief 时钟信号的频率
-        /// @return
-        virtual base::Hz Frequency() const = 0;
+		/// @brief 时钟信号的频率
+		/// @return
+		virtual base::MHz Frequency() const = 0;
 
-        /// @brief 打开时钟信号。
-        /// @note 有的时钟信号只有输入分频，没有输出分频，就使用本重载。
-        /// @param input_division_factor
-        virtual void Open(bsp::IClockSignal_InputDivisionFactor const &input_division_factor);
+		/// @brief 打开时钟信号。
+		/// @note 有的时钟信号只有输入分频，没有输出分频，就使用本重载。
+		/// @param input_division_factor
+		virtual void Open(bsp::IClockSignal_InputDivisionFactor const &input_division_factor);
 
-        /// @brief 打开时钟信号。
-        /// @note 有的时钟信号只有输出分频，没有输入分频，就使用本重载。
-        /// @param output_division_factor
-        virtual void Open(bsp::IClockSignal_OutputDivisionFactor const &output_division_factor);
+		/// @brief 打开时钟信号。
+		/// @note 有的时钟信号只有输出分频，没有输入分频，就使用本重载。
+		/// @param output_division_factor
+		virtual void Open(bsp::IClockSignal_OutputDivisionFactor const &output_division_factor);
 
-        /// @brief 打开时钟信号。
-        /// @param output_division_factor 输出分频系数。
-        /// @param clock_source 时钟源。像 stm32 的系统时钟 sysclk，是时钟源后的第一个时钟信号，输入端连接着
-        /// 各个时钟源，输出端供给各个子时钟信号。本参数用来选择类似 sysclk 这种时钟信号的时钟源。
-        virtual void Open(bsp::IClockSignal_OutputDivisionFactor const &output_division_factor,
-                          IClockSignal_ClockSource const &clock_source);
+		/// @brief 打开时钟信号。
+		/// @param output_division_factor 输出分频系数。
+		/// @param clock_source 时钟源。像 stm32 的系统时钟 sysclk，是时钟源后的第一个时钟信号，输入端连接着
+		/// 各个时钟源，输出端供给各个子时钟信号。本参数用来选择类似 sysclk 这种时钟信号的时钟源。
+		virtual void Open(bsp::IClockSignal_OutputDivisionFactor const &output_division_factor,
+						  IClockSignal_ClockSource const &clock_source);
 
-        /// @brief 打开时钟信号。
-        /// @note 有的时钟信号既有输入分频，又有输出分频，就使用本重载。
-        /// @param input_division_factor
-        /// @param output_division_factor
-        virtual void Open(bsp::IClockSignal_InputDivisionFactor const &input_division_factor,
-                          bsp::IClockSignal_OutputDivisionFactor const &output_division_factor);
-    };
+		/// @brief 打开时钟信号。
+		/// @note 有的时钟信号既有输入分频，又有输出分频，就使用本重载。
+		/// @param input_division_factor
+		/// @param output_division_factor
+		virtual void Open(bsp::IClockSignal_InputDivisionFactor const &input_division_factor,
+						  bsp::IClockSignal_OutputDivisionFactor const &output_division_factor);
+	};
 } // namespace bsp
--- include/bsp-interface/clock/IClockSource.cpp
+++ include/bsp-interface/clock/IClockSource.cpp
@@ -4,46 +4,47 @@
 
 void bsp::IClockSource::Open()
 {
-    throw std::runtime_error{"不支持此重载版本。"};
+	throw std::runtime_error{"不支持此重载版本。"};
 }
 
-void bsp::IClockSource::Open(base::Hz const &crystal_frequency)
+void bsp::IClockSource::Open(base::MHz const &crystal_frequency)
 {
-    throw std::runtime_error{"不支持此重载版本。"};
+	throw std::runtime_error{"不支持此重载版本。"};
 }
 
 void bsp::IClockSource::Open(std::string const &input_channel_name)
 {
-    throw std::runtime_error{"不支持此重载版本。"};
+	throw std::runtime_error{"不支持此重载版本。"};
 }
 
 void bsp::IClockSource::Open(std::string const &input_channel_name, base::IDictionary<std::string, int> const &factors)
 {
-    throw std::runtime_error{"不支持此重载版本。"};
+	throw std::runtime_error{"不支持此重载版本。"};
 }
 
 #pragma endregion
 
 #pragma region SetAsBypass
 
-void bsp::IClockSource::SetAsBypass(base::Hz external_clock_frequency)
+void bsp::IClockSource::SetAsBypass(base::MHz external_clock_frequency)
 {
-    throw std::runtime_error{"不支持此重载版本。"};
+	throw std::runtime_error{"不支持此重载版本。"};
 }
 
-void bsp::IClockSource::SetAsBypass(base::Hz external_clock_frequency, base::IDictionary<std::string, base::Hz> const &target_output_frequencies)
+void bsp::IClockSource::SetAsBypass(base::MHz external_clock_frequency,
+									base::IDictionary<std::string, base::MHz> const &target_output_frequencies)
 {
-    throw std::runtime_error{"不支持此重载版本。"};
+	throw std::runtime_error{"不支持此重载版本。"};
 }
 
 #pragma endregion
 
-base::Hz bsp::IClockSource::Frequency() const
+base::MHz bsp::IClockSource::Frequency() const
 {
-    throw std::runtime_error{"不支持此重载版本。"};
+	throw std::runtime_error{"不支持此重载版本。"};
 }
 
-base::Hz bsp::IClockSource::Frequency(std::string const &output_channel_name) const
+base::MHz bsp::IClockSource::Frequency(std::string const &output_channel_name) const
 {
-    throw std::runtime_error{"不支持此重载版本。"};
+	throw std::runtime_error{"不支持此重载版本。"};
 }
