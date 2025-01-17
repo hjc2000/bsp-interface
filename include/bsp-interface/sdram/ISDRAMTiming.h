67c65734af8a5e2dc3044eeffdb7a9bdb9573a9a
hjc2000 <hjc.666@foxmail.com>
2025-01-17 14:59:48 +0800

    ISDRAMTiming

--- include/bsp-interface/sdram/ISDRAMTiming.h
+++ include/bsp-interface/sdram/ISDRAMTiming.h
@@ -11,11 +11,6 @@ namespace bsp
 		/// @return
 		virtual base::Nanoseconds T_CLK() = 0;
 
-		/// @brief 行循环时间。
-		/// @note 本次激活一行到下一次激活一行的时间间隔。
-		/// @return
-		virtual base::Nanoseconds T_RC() = 0;
-
 		/// @brief 模式寄存器设置延迟。
 		/// @note 设置模式寄存器后要等待这么长时间才可以进行下一个命令。
 		/// 	@li 设置模式寄存器后要等待 T_RSC 后才可以进行下一次的设置模式寄存器。
@@ -23,6 +18,27 @@ namespace bsp
 		/// 	@li 设置模式寄存器后要等待 T_RSC 后才可以发送自动刷新命令。
 		/// @return
 		virtual base::Nanoseconds T_RSC() = 0;
+
+		/// @brief 退出自刷新模式的时间。
+		/// @note 注意：“自刷新” 不是 “自动刷新”
+		/// @return
+		virtual base::Nanoseconds T_XSR() = 0;
+
+		/// @brief 从激活到预充电的时间。
+		/// @note 激活一行后，至少保持 T_RAS 后才能发送预充电命令关闭这行。
+		/// @return
+		virtual base::Nanoseconds T_RAS() = 0;
+
+		/// @brief 行循环时间。
+		/// @note 本次激活一行到下一次激活一行的时间间隔。
+		/// @return
+		virtual base::Nanoseconds T_RC() = 0;
+
+		/// @brief 写恢复时间。
+		/// @note 发送写命令后，至少将信号保持 T_WR 后才能撤掉信号进入下一个命令。下一个命令可以是写命令，
+		/// 也可以是预充电命令。如果使用了自动预充电的写模式，则 SDRAM 内部会自己在 T_WR 后进行预充电。
+		/// @return
+		virtual base::Nanoseconds T_WR() = 0;
 
 		/// @brief 自动刷新时间。
 		/// @note 至少这么长时间刷新一次所有行。如果超过这么长时间没有刷新完所有行，数据有可能丢失。
