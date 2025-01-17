925caf08c7d8536bec35410236ed359b021b9214
hjc2000 <hjc.666@foxmail.com>
2025-01-17 17:26:18 +0800

    Ceil

--- include/bsp-interface/sdram/ISDRAMTiming.cpp
+++ include/bsp-interface/sdram/ISDRAMTiming.cpp
@@ -10,15 +10,15 @@ base::Json bsp::ISDRAMTiming::ToJson() const
 	base::Json root{
 		{"RowCount", RowCount()},
 		{"T_CLK", T_CLK().ToString()},
-		{"T_RSC", static_cast<base::Fraction>(T_RSC() / T_CLK()).ToString()},
-		{"T_XSR", static_cast<base::Fraction>(T_XSR() / T_CLK()).ToString()},
-		{"T_RAS", static_cast<base::Fraction>(T_RAS() / T_CLK()).ToString()},
-		{"T_RC", static_cast<base::Fraction>(T_RC() / T_CLK()).ToString()},
-		{"T_WR", static_cast<base::Fraction>(T_WR() / T_CLK()).ToString()},
-		{"T_RP", static_cast<base::Fraction>(T_RP() / T_CLK()).ToString()},
-		{"T_RCD", static_cast<base::Fraction>(T_RCD() / T_CLK()).ToString()},
-		{"T_REF", static_cast<base::Fraction>(T_REF() / T_CLK()).ToString()},
-		{"T_AutoRefreshCommand", static_cast<base::Fraction>(T_AutoRefreshCommand() / T_CLK()).ToString()},
+		{"T_RSC", static_cast<base::Fraction>(T_RSC() / T_CLK()).Ceil()},
+		{"T_XSR", static_cast<base::Fraction>(T_XSR() / T_CLK()).Ceil()},
+		{"T_RAS", static_cast<base::Fraction>(T_RAS() / T_CLK()).Ceil()},
+		{"T_RC", static_cast<base::Fraction>(T_RC() / T_CLK()).Ceil()},
+		{"T_WR", static_cast<base::Fraction>(T_WR() / T_CLK()).Ceil()},
+		{"T_RP", static_cast<base::Fraction>(T_RP() / T_CLK()).Ceil()},
+		{"T_RCD", static_cast<base::Fraction>(T_RCD() / T_CLK()).Ceil()},
+		{"T_REF", static_cast<base::Fraction>(T_REF() / T_CLK()).Ceil()},
+		{"T_AutoRefreshCommand", static_cast<base::Fraction>(T_AutoRefreshCommand() / T_CLK()).Ceil()},
 	};
 
 	return root;
