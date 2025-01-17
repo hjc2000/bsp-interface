e722ba31cef5d56552dbf855647c6039d4816557
hjc2000 <hjc.666@foxmail.com>
2025-01-17 10:50:29 +0800

    namespace

--- include/bsp-interface/di/ethernet.h
+++ include/bsp-interface/di/ethernet.h
@@ -2,13 +2,22 @@
 #include <bsp-interface/ethernet/IEthernetController.h>
 #include <bsp-interface/ethernet/IEthernetPort.h>
 
-/// @brief 以太网控制器。
-/// @note 负责单片机外设的人实现此函数。
-/// @return
-bsp::IEthernetController &DI_EthernetController();
+namespace bsp
+{
+	namespace di
+	{
+		namespace ethernet
+		{
+			/// @brief 以太网控制器。
+			/// @note 负责单片机外设的人实现此函数。
+			/// @return
+			bsp::IEthernetController &EthernetController();
 
-/// @brief 以太网端口。
-/// @note 负责 PHY 的人，利用 DI_EthernetController 抽象出来的单片机的以太网控制器来操作 PHY，
-/// 实现本函数。
-/// @return
-bsp::IEthernetPort &DI_EthernetPort();
+			/// @brief 以太网端口。
+			/// @note 负责 PHY 的人，利用 DI_EthernetController 抽象出来的单片机的以太网控制器来操作 PHY，
+			/// 实现本函数。
+			/// @return
+			bsp::IEthernetPort &EthernetPort();
+		} // namespace ethernet
+	} // namespace di
+} // namespace bsp
