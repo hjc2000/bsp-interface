0f1710ed205f4322af52bdf3384d28936eb8b3ba
hjc2000 <hjc.666@foxmail.com>
2025-01-17 15:23:53 +0800

    ISDRAM

---
+++ include/bsp-interface/sdram/ISDRAM.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <base/stream/Span.h>
+#include <bsp-interface/sdram/ISDRAMTiming.h>
+
+namespace bsp
+{
+	/// @brief SDRAM 接口。
+	class ISDRAM
+	{
+	public:
+		/// @brief 打开 SDRAM.
+		/// @param timing
+		virtual void Open(bsp::ISDRAMTiming const &timing) = 0;
+
+		/// @brief 此 SDRAM 的内存段。
+		/// @return
+		virtual base::Span Span() = 0;
+	};
+} // namespace bsp
