71e288fa9fedb5e2536c7653c650b1a388bd08a5
hjc2000 <hjc.666@foxmail.com>
2025-01-17 17:24:26 +0800

    base::Json bsp::ISDRAMTiming::ToJson() const

--- include/bsp-interface/sdram/ISDRAMTiming.cpp
+++ include/bsp-interface/sdram/ISDRAMTiming.cpp
@@ -8,17 +8,17 @@ base::Nanoseconds bsp::ISDRAMTiming::T_AutoRefreshCommand() const
 base::Json bsp::ISDRAMTiming::ToJson() const
 {
 	base::Json root{
-		{"T_CLK", T_CLK().ToString()},
-		{"T_RSC", T_RSC().ToString()},
-		{"T_XSR", T_XSR().ToString()},
-		{"T_RAS", T_RAS().ToString()},
-		{"T_RC", T_RC().ToString()},
-		{"T_WR", T_WR().ToString()},
-		{"T_RP", T_RP().ToString()},
-		{"T_RCD", T_RCD().ToString()},
-		{"T_REF", T_REF().ToString()},
 		{"RowCount", RowCount()},
-		{"T_AutoRefreshCommand", T_AutoRefreshCommand().ToString()},
+		{"T_CLK", T_CLK().ToString()},
+		{"T_RSC", static_cast<base::Fraction>(T_RSC() / T_CLK()).ToString()},
+		{"T_XSR", static_cast<base::Fraction>(T_XSR() / T_CLK()).ToString()},
+		{"T_RAS", static_cast<base::Fraction>(T_RAS() / T_CLK()).ToString()},
+		{"T_RC", static_cast<base::Fraction>(T_RC() / T_CLK()).ToString()},
+		{"T_WR", static_cast<base::Fraction>(T_WR() / T_CLK()).ToString()},
+		{"T_RP", static_cast<base::Fraction>(T_RP() / T_CLK()).ToString()},
+		{"T_RCD", static_cast<base::Fraction>(T_RCD() / T_CLK()).ToString()},
+		{"T_REF", static_cast<base::Fraction>(T_REF() / T_CLK()).ToString()},
+		{"T_AutoRefreshCommand", static_cast<base::Fraction>(T_AutoRefreshCommand() / T_CLK()).ToString()},
 	};
 
 	return root;
