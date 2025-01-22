b891699ac90b7760d8a3058fe8374095aa2c3f72
hjc2000 <hjc.666@foxmail.com>
2025-01-22 15:40:38 +0800

    GetTiming

--- include/bsp-interface/sdram/chip/W9825G6KH_6_TimingProvider.cpp
+++ include/bsp-interface/sdram/chip/W9825G6KH_6_TimingProvider.cpp
@@ -1,1 +1,12 @@
 #include "W9825G6KH_6_TimingProvider.h"
+#include <W9825G6KH_6_Timing.h>
+
+std::shared_ptr<bsp::sdram::ISDRAMTiming> bsp::sdram::chip::W9825G6KH_6_TimingProvider::GetTiming(base::MHz const &clk_frequency) const
+{
+	if (clk_frequency > MaxClkFrequency())
+	{
+		throw std::invalid_argument{"时钟频率超过最大值"};
+	}
+
+	return std::shared_ptr<bsp::sdram::ISDRAMTiming>{new bsp::sdram::chip::W9825G6KH_6_Timing{clk_frequency}};
+}
--- include/bsp-interface/sdram/chip/W9825G6KH_6_TimingProvider.h
+++ include/bsp-interface/sdram/chip/W9825G6KH_6_TimingProvider.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <bsp-interface/sdram/chip/W9825G6KH_6_Timing.h>
 #include <bsp-interface/sdram/ISDRAMTimingProvider.h>
 #include <stdexcept>
 
@@ -23,15 +22,7 @@ namespace bsp
 				/// @brief 获取指定 CLK 频率下的时序。
 				/// @param clk_frequency
 				/// @return
-				virtual std::shared_ptr<bsp::sdram::ISDRAMTiming> GetTiming(base::MHz const &clk_frequency) const override
-				{
-					if (clk_frequency > MaxClkFrequency())
-					{
-						throw std::invalid_argument{"时钟频率超过最大值"};
-					}
-
-					return std::shared_ptr<bsp::sdram::ISDRAMTiming>{new bsp::sdram::chip::W9825G6KH_6_Timing{clk_frequency}};
-				}
+				virtual std::shared_ptr<bsp::sdram::ISDRAMTiming> GetTiming(base::MHz const &clk_frequency) const override;
 			};
 		} // namespace chip
 	} // namespace sdram
