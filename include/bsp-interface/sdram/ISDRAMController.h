cdbb6135511e5f3a1c7b448ff0a184caf4f1aebc
hjc2000 <hjc.666@foxmail.com>
2025-01-22 11:27:42 +0800

    ISDRAMController

--- include/bsp-interface/sdram/ISDRAMController.h
+++ include/bsp-interface/sdram/ISDRAMController.h
@@ -6,14 +6,70 @@ namespace bsp
 {
 	namespace sdram
 	{
+		namespace property
+		{
+			class RowCount
+			{
+			public:
+				explicit RowCount(int value)
+					: _value(value)
+				{
+				}
+
+				int _value = 0;
+			};
+
+			class ColumnCount
+			{
+			public:
+				explicit ColumnCount(int value)
+					: _value(value)
+				{
+				}
+
+				int _value = 0;
+			};
+
+			class DataWidth
+			{
+			public:
+				explicit DataWidth(int value)
+					: _value(value)
+				{
+				}
+
+				int _value = 0;
+			};
+
+			/// @brief 读突发长度
+			class ReadBurstLength
+			{
+			public:
+				explicit ReadBurstLength(int value)
+					: _value(value)
+				{
+				}
+
+				int _value = 0;
+			};
+		} // namespace property
+
 		/// @brief SDRAM 控制器接口。
 		/// @note 这个接口用来封装类似于 stm32 的 FMC 接口的 SDRAM 控制器。
 		class ISDRAMController
 		{
 		public:
-			/// @brief 打开 SDRAM 控制器。
+			/// @brief 将 SDRAM 控制器以读突发的模式打开。写不突发。
 			/// @param timing
-			virtual void Open(bsp::sdram::ISDRAMTiming const &timing) = 0;
+			/// @param row_count
+			/// @param column_count
+			/// @param data_width
+			/// @param read_burst_length
+			virtual void OpenAsReadBurstMode(bsp::sdram::ISDRAMTiming const &timing,
+											 bsp::sdram::property::RowCount const &row_count,
+											 bsp::sdram::property::ColumnCount const &column_count,
+											 bsp::sdram::property::DataWidth const &data_width,
+											 bsp::sdram::property::ReadBurstLength const &read_burst_length) = 0;
 		};
 	} // namespace sdram
 } // namespace bsp
