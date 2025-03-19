36aa4de2bd848b08d9b11a4aba59ca6309e87e0e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-20 00:51:26 +0800

    TaskSingletonGetter

--- include/bsp-interface/TaskSingletonGetter.h
+++ include/bsp-interface/TaskSingletonGetter.h
@@ -1,21 +1,24 @@
 #pragma once
-#include <base/di/SingletonGetter.h>
-#include <base/task/IMutex.h>
-#include <bsp-interface/di/task.h>
+#include "base/LockGuard.h"
+#include "base/task/IMutex.h"
+#include "bsp-interface/di/task.h"
 
 namespace bsp
 {
+	///
 	/// @brief 在任务函数中适用的单例获取器，避免多个任务同时获取一个单例造成
 	/// 多次调用其构造函数。
+	///
 	/// @tparam T
+	///
 	template <typename T>
 	class TaskSingletonGetter :
-		public base::SingletonGetter<T>
+		protected base::ILock
 	{
 	private:
 		inline static_field std::shared_ptr<base::IMutex> _lock = nullptr;
+		inline static_field std::unique_ptr<T> _p;
 
-	protected:
 		virtual void Lock() override
 		{
 			{
@@ -32,6 +35,57 @@ namespace bsp
 		virtual void Unlock() override
 		{
 			_lock->Unlock();
+		}
+
+	protected:
+		virtual std::unique_ptr<T> Create() = 0;
+
+	public:
+		///
+		/// @brief 获取单例的引用
+		///
+		/// @return T&
+		///
+		T &Instance()
+		{
+			if (_p != nullptr)
+			{
+				return *_p;
+			}
+
+			base::LockGuard l{*this};
+			if (_p != nullptr)
+			{
+				return *_p;
+			}
+
+			_p = Create();
+			if (_p == nullptr)
+			{
+				throw std::runtime_error{"禁止将 Create 函数实现为返回空指针。"};
+			}
+
+			return *_p;
+		}
+
+		///
+		/// @brief 重载解引用运算符，用来获取单例的引用。
+		///
+		/// @return T&
+		///
+		T &operator*()
+		{
+			return Instance();
+		}
+
+		///
+		/// @brief 重载指针式的成员访问运算符，用来访问单例对象的成员。
+		///
+		/// @return T*
+		///
+		T *operator->()
+		{
+			return &Instance();
 		}
 	};
 } // namespace bsp
