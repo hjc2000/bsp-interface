4021e4af89d294a639bf27fe1eed258f72f48cef
hjc2000 <hjc.666@foxmail.com>
2025-03-31 09:53:18 +0800

    IGpioPin

--- include/bsp-interface/gpio/IGpioPin.h
+++ include/bsp-interface/gpio/IGpioPin.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "base/container/iterator/IEnumerable.h"
 #include <functional>
 #include <string>
 
@@ -129,18 +128,6 @@ namespace bsp
 		/// @return
 		///
 		virtual std::string PinName() const = 0;
-
-		///
-		/// @brief 支持的复用功能。
-		///
-		/// @note 复用功能是非常复杂的，各个型号单片机都不一样，所以采用字符串。
-		///
-		/// @note 假设某个单片机的一个引脚的复用功能有：uart1, timer1_compare_output 等。
-		/// 这种名称是与具体型号高度相关的，所以本库无法提供一个枚举来列举这些情况。
-		///
-		/// @return
-		///
-		virtual base::IEnumerable<std::string> &SupportedAlternateFunctions() = 0;
 
 		///
 		/// @brief 指示此引脚是否已经打开。
