fc8ca8786519ea17f4fe131e4eea7b4c3019c00e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-06 01:20:22 +0800

    空行

--- project.cmake
+++ project.cmake
@@ -1,5 +1,6 @@
 add_library(${ProjectName} STATIC)
 target_import_src(${ProjectName})
+
 target_import_boost(${ProjectName} PUBLIC)
 target_import_nlohmann_json(${ProjectName} PUBLIC)
 target_import_base(${ProjectName} PUBLIC)
