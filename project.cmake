7b6c0b2336492f823100d57c8b23bd40af9fadbd
hjc2000 <hjc.666@foxmail.com>
2025-04-07 09:20:31 +0800

    OBJECT

--- project.cmake
+++ project.cmake
@@ -1,5 +1,6 @@
-add_library(${ProjectName} STATIC)
+add_library(${ProjectName} OBJECT)
 target_import_src(${ProjectName})
+target_install_obj_dir(${ProjectName})
 
 target_import_boost(${ProjectName} PUBLIC)
 target_import_nlohmann_json(${ProjectName} PUBLIC)
