9eef195db2af5084d1e380c10ad2281078e1eb3f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-09 20:56:47 +0800

    将dma的枚举移动到base库中

--- include/bsp-interface/dma/IDmaChannel.h
+++ include/bsp-interface/dma/IDmaChannel.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "base/peripheral/IDma.h"
 #include <base/string/define.h>
 #include <stdexcept>
 #include <string>
@@ -7,67 +8,6 @@ namespace bsp
 {
 	namespace dma
 	{
-		/// @brief DMA 优先级
-		enum class Priority
-		{
-			Low,
-			Medium,
-			High,
-			VeryHigh,
-		};
-
-		/// @brief DMA 在读写外设地址空间时，是否自动递增地址。即每读取 1 个字节，
-		/// 就将地址递增 1.
-		enum class PeripheralIncrement
-		{
-			Increase,
-			DoNotIncrease,
-		};
-
-		/// @brief DMA 在读写内存地址空间时是否自动递增地址。即每读取 1 个字节，
-		/// 就将地址递增 1.
-		enum class MemoryIncrement
-		{
-			Increase,
-			DoNotIncrease,
-		};
-
-		/// @brief DMA 读写外设时采用的数据对齐字节数。例如 1 字节对齐或 4 字节对齐。
-		class PeripheralDataAlignment
-		{
-		private:
-			int _value = 4;
-
-		public:
-			explicit PeripheralDataAlignment(int value)
-			{
-				_value = value;
-			}
-
-			int Value() const
-			{
-				return _value;
-			}
-		};
-
-		/// @brief DMA 读写内存时采用的数据对齐字节数。例如 1 字节对齐或 4 字节对齐。
-		class MemoryDataAlignment
-		{
-		private:
-			int _value = 4;
-
-		public:
-			explicit MemoryDataAlignment(int value)
-			{
-				_value = value;
-			}
-
-			int Value() const
-			{
-				return _value;
-			}
-		};
-
 		/// @brief DMA 通道
 		class IDmaChannel
 		{
@@ -83,11 +23,11 @@ namespace bsp
 			/// @param priority
 			/// @param request
 			virtual void OpenAsPeripheralToMemoryMode(void *parent,
-													  bsp::dma::PeripheralIncrement peripheral_increment,
-													  bsp::dma::MemoryIncrement memory_increment,
-													  bsp::dma::PeripheralDataAlignment const &peripheral_data_alignment,
-													  bsp::dma::MemoryDataAlignment const &memory_data_alignment,
-													  bsp::dma::Priority priority,
+													  base::dma::PeripheralIncrement peripheral_increment,
+													  base::dma::MemoryIncrement memory_increment,
+													  base::dma::PeripheralDataAlignment const &peripheral_data_alignment,
+													  base::dma::MemoryDataAlignment const &memory_data_alignment,
+													  base::dma::Priority priority,
 													  std::string const &request)
 			{
 				throw std::runtime_error{CODE_POS_STR + "不支持此模式"};
@@ -102,11 +42,11 @@ namespace bsp
 			/// @param priority
 			/// @param request
 			virtual void OpenAsMemoryToPeripheralMode(void *parent,
-													  bsp::dma::PeripheralIncrement peripheral_increment,
-													  bsp::dma::MemoryIncrement memory_increment,
-													  bsp::dma::PeripheralDataAlignment const &peripheral_data_alignment,
-													  bsp::dma::MemoryDataAlignment const &memory_data_alignment,
-													  bsp::dma::Priority priority,
+													  base::dma::PeripheralIncrement peripheral_increment,
+													  base::dma::MemoryIncrement memory_increment,
+													  base::dma::PeripheralDataAlignment const &peripheral_data_alignment,
+													  base::dma::MemoryDataAlignment const &memory_data_alignment,
+													  base::dma::Priority priority,
 													  std::string const &request)
 			{
 				throw std::runtime_error{CODE_POS_STR + "不支持此模式"};
@@ -121,11 +61,11 @@ namespace bsp
 			/// @param priority
 			/// @param request
 			virtual void OpenAsMomoryToMemoryMode(void *parent,
-												  bsp::dma::PeripheralIncrement peripheral_increment,
-												  bsp::dma::MemoryIncrement memory_increment,
-												  bsp::dma::PeripheralDataAlignment const &peripheral_data_alignment,
-												  bsp::dma::MemoryDataAlignment const &memory_data_alignment,
-												  bsp::dma::Priority priority,
+												  base::dma::PeripheralIncrement peripheral_increment,
+												  base::dma::MemoryIncrement memory_increment,
+												  base::dma::PeripheralDataAlignment const &peripheral_data_alignment,
+												  base::dma::MemoryDataAlignment const &memory_data_alignment,
+												  base::dma::Priority priority,
 												  std::string const &request)
 			{
 				throw std::runtime_error{CODE_POS_STR + "不支持此模式"};
