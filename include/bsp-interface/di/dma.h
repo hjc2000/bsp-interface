079975cd6291b52441fa35dc4021a32ad0dd5133
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-10 00:06:10 +0800

    删除dma

--- include/bsp-interface/di/dma.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <base/container/IDictionary.h>
-#include <bsp-interface/dma/IDmaChannel.h>
-#include <memory>
-
-/// @brief DMA 通道集合。
-/// @return
-base::IDictionary<std::string, bsp::dma::IDmaChannel *> const &DI_DmaChannelCollection();
