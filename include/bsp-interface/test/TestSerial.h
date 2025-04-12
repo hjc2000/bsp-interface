804b44ed03d6c2bb7b0a15ca1bc1ef016ecae637
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-13 01:55:03 +0800

    删除ISerial

--- include/bsp-interface/test/TestSerial.h
+++ include/bsp-interface/test/TestSerial.h
@@ -1,27 +1,21 @@
 #pragma once
 #include "base/peripheral/led/IDigitalLed.h"
-#include "base/peripheral/serial/ISerial.h"
-#include <stdexcept>
+#include "base/peripheral/serial/Serial.h"
 
 namespace bsp
 {
 	inline void TestSerial()
 	{
-		base::serial::ISerial *serial = base::serial::SerialCollection().Get("serial");
-		if (serial == nullptr)
-		{
-			throw std::runtime_error{"找不到名为 serial 的串口"};
-		}
-
+		base::serial::Serial serial{1};
 		base::led::RedDigitalLed().TurnOn();
-		serial->Start();
+		serial.Start();
 		std::unique_ptr<uint8_t[]> buffer{new uint8_t[128]};
 		while (true)
 		{
-			int32_t have_read = serial->Read(buffer.get(), 0, 128);
+			int32_t have_read = serial.Read(buffer.get(), 0, 128);
 			base::led::GreenDigitalLed().Toggle();
 			base::led::RedDigitalLed().Toggle();
-			serial->Write(buffer.get(), 0, have_read);
+			serial.Write(buffer.get(), 0, have_read);
 		}
 	}
 } // namespace bsp
