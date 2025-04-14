d744b1f13a3ae333702419d8f3f4a4aac159201d
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-14 23:12:02 +0800

    Value()

--- include/bsp-interface/sdram/ISDRAMController.h
+++ include/bsp-interface/sdram/ISDRAMController.h
@@ -9,61 +9,91 @@ namespace bsp
 		/// @brief SDRAM 中 BANK 的数量。
 		class BankCount
 		{
+		private:
+			int _value = 0;
+
 		public:
 			explicit BankCount(int value)
 				: _value(value)
 			{
 			}
 
-			int _value = 0;
+			int Value() const
+			{
+				return _value;
+			}
 		};
 
 		/// @brief 行地址的位数。
 		class RowBitCount
 		{
+		private:
+			int _value = 0;
+
 		public:
 			explicit RowBitCount(int value)
 				: _value(value)
 			{
 			}
 
-			int _value = 0;
+			int Value() const
+			{
+				return _value;
+			}
 		};
 
 		/// @brief 列地址的位数。
 		class ColumnBitCount
 		{
+		private:
+			int _value = 0;
+
 		public:
 			explicit ColumnBitCount(int value)
 				: _value(value)
 			{
 			}
 
-			int _value = 0;
+			int Value() const
+			{
+				return _value;
+			}
 		};
 
 		/// @brief 数据宽度，或者说数据总线位数。
 		class DataWidth
 		{
+		private:
+			int _value = 0;
+
 		public:
 			explicit DataWidth(int value)
 				: _value(value)
 			{
 			}
 
-			int _value = 0;
+			int Value() const
+			{
+				return _value;
+			}
 		};
 
 		/// @brief 读突发长度
 		class ReadBurstLength
 		{
+		private:
+			int _value = 0;
+
 		public:
 			explicit ReadBurstLength(int value)
 				: _value(value)
 			{
 			}
 
-			int _value = 0;
+			int Value() const
+			{
+				return _value;
+			}
 		};
 
 		/// @brief SDRAM 控制器接口。
