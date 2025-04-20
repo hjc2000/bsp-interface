b6d924e9845057efc9cae80d28b8a675c718a5b1
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-20 12:50:51 +0800

    去除控制台打印

--- include/bsp-interface/ethernet/phy/LAN8720APhyDriver.cpp
+++ include/bsp-interface/ethernet/phy/LAN8720APhyDriver.cpp
@@ -1,8 +1,5 @@
 #include "LAN8720APhyDriver.h"
-#include <base/container/Array.h>
-#include <base/string/define.h>
-#include <base/unit/Mbps.h>
-#include <bsp-interface/di/console.h>
+#include "base/unit/Mbps.h"
 
 bsp::LAN8720APhyDriver::LAN8720APhyDriver(base::ethernet::EthernetController const &controller)
 	: _ethernet_controller(controller)
@@ -20,11 +17,9 @@ base::ethernet::DuplexMode bsp::LAN8720APhyDriver::DuplexMode()
 	uint32_t const mask = 0b10000;
 	if (register_value & mask)
 	{
-		bsp::di::Console().WriteLine("全双工");
 		return base::ethernet::DuplexMode::FullDuplex;
 	}
 
-	bsp::di::Console().WriteLine("半双工");
 	return base::ethernet::DuplexMode::HalfDuplex;
 }
 
@@ -34,10 +29,8 @@ base::bps bsp::LAN8720APhyDriver::Speed()
 	uint32_t const mask = 0b01000;
 	if (register_value & mask)
 	{
-		bsp::di::Console().WriteLine("连接速率：100 Mbps");
 		return base::bps{base::Mbps{100}};
 	}
 
-	bsp::di::Console().WriteLine("连接速率：10 Mbps");
 	return base::bps{base::Mbps{10}};
 }
--- include/bsp-interface/ethernet/phy/YT8512CPhyDriver.cpp
+++ include/bsp-interface/ethernet/phy/YT8512CPhyDriver.cpp
@@ -1,9 +1,7 @@
 #include "YT8512CPhyDriver.h"
 #include "base/peripheral/ethernet/parameter.h"
-#include <base/container/Array.h>
-#include <base/string/define.h>
-#include <base/unit/Mbps.h>
-#include <bsp-interface/di/console.h>
+#include "base/string/define.h"
+#include "base/unit/Mbps.h"
 
 bsp::YT8512CPhyDriver::YT8512CPhyDriver(base::ethernet::EthernetController const &controller)
 	: _ethernet_controller(controller)
@@ -23,11 +21,9 @@ base::ethernet::DuplexMode bsp::YT8512CPhyDriver::DuplexMode()
 
 	if (duplex_register)
 	{
-		bsp::di::Console().WriteLine("全双工");
 		return base::ethernet::DuplexMode::FullDuplex;
 	}
 
-	bsp::di::Console().WriteLine("半双工");
 	return base::ethernet::DuplexMode::HalfDuplex;
 }
 
@@ -39,21 +35,18 @@ base::bps bsp::YT8512CPhyDriver::Speed()
 
 	if (duplex_register == 0b00)
 	{
-		bsp::di::Console().WriteLine("连接速率：10 Mbps");
 		return base::bps{base::Mbps{10}};
 	}
 	else if (duplex_register == 0b01)
 	{
-		bsp::di::Console().WriteLine("连接速率：100 Mbps");
 		return base::bps{base::Mbps{100}};
 	}
 	else if (duplex_register == 0b01)
 	{
-		bsp::di::Console().WriteLine("连接速率：1000 Mbps");
 		return base::bps{base::Mbps{1000}};
 	}
 	else
 	{
-		throw std::runtime_error{"读取连接速率寄存器时读取到保留值。"};
+		throw std::runtime_error{CODE_POS_STR + "读取连接速率寄存器时读取到保留值。"};
 	}
 }
