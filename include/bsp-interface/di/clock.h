95a19780d6a3e44946682aea27b514396b42fc28
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-05-04 13:07:29 +0800

    删除IClockSource

--- include/bsp-interface/di/clock.h
+++ include/bsp-interface/di/clock.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <base/container/IDictionary.h>
 #include <bsp-interface/clock/IClockSignal.h>
-#include <bsp-interface/clock/IClockSource.h>
 #include <string>
 
 namespace bsp
@@ -10,10 +9,6 @@ namespace bsp
 	{
 		namespace clock
 		{
-			/// @brief 时钟源集合。
-			/// @return
-			base::IDictionary<std::string, bsp::IClockSource *> const &ClockSourceCollection();
-
 			/// @brief 时钟信号集合。
 			/// @return
 			base::IDictionary<std::string, bsp::IClockSignal *> const &ClockSignalCollection();
