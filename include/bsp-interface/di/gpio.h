ac76cdeb40ac72ffe76f48e7aafe9cc36f182807
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-05-05 23:41:20 +0800

    删除旧版GPIO接口

--- include/bsp-interface/di/gpio.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <base/container/IDictionary.h>
-#include <bsp-interface/gpio/IGpioPin.h>
-
-/// @brief GPIO 引脚集合
-/// @return
-base::IDictionary<std::string, bsp::IGpioPin *> const &DI_GpioPinCollection();
