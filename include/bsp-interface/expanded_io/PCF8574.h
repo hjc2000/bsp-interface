eb53f4866be1b8b4878d3821849ba3a378d0ba88
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-05-05 23:19:54 +0800

    内联

--- include/bsp-interface/expanded_io/PCF8574.h
+++ include/bsp-interface/expanded_io/PCF8574.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "base/embedded/iic/IicHost.h"
-#include <bsp-interface/expanded_io/IExpandedIoPort.h>
-#include <bsp-interface/gpio/IGpioPin.h>
+#include "base/task/delay.h"
+#include "bsp-interface/expanded_io/IExpandedIoPort.h"
+#include "bsp-interface/gpio/IGpioPin.h"
 #include <functional>
 #include <memory>
 
@@ -40,27 +41,79 @@ namespace bsp
 		PCF8574(std::string const &name,
 				bsp::IGpioPin *interrupt_pin,
 				std::shared_ptr<base::iic::IicHost> const &iic_host,
-				uint8_t address);
+				uint8_t address)
+		{
+			_name = name;
+			_interrupt_pin = interrupt_pin;
+			_iic_host = iic_host;
 
-		std::string Name() const override;
+			if (address > 0b111)
+			{
+				throw std::out_of_range{"地址超出范围。允许的地址范围为 [0, 7]"};
+			}
+
+			_address_register = 0b01000000 | (address << 1);
+
+			// 打开中断引脚
+			_interrupt_pin->OpenAsInputMode(bsp::IGpioPinPullMode::PullUp,
+											bsp::IGpioPinTriggerEdge::FallingEdge);
+
+			// 初始化后将所有引脚置为高电平。即让芯片内每个引脚的开关管关断。
+			WriteByte(0, 0xff);
+		}
+
+		std::string Name() const override
+		{
+			return _name;
+		}
 
 		/// @brief 注册中断回调函数。
 		/// @param func
-		void RegisterInterruptCallback(std::function<void()> func) override;
+		void RegisterInterruptCallback(std::function<void()> func) override
+		{
+			_interrupt_pin->RegisterInterruptCallback(func);
+		}
 
 		/// @brief 取消注册中断回调函数。
-		void UnregisterInterruptCallback() override;
+		void UnregisterInterruptCallback() override
+		{
+			_interrupt_pin->UnregisterInterruptCallback();
+		}
 
 		/// @brief 读取一个字节。这是一个 8 位的 IO 扩展芯片，读取 1 个字节意味着读取
 		/// 所有 IO 端子的电平。
 		/// @param index 索引。要读取第几个字节。
 		/// @return
-		uint8_t ReadByte(int index) override;
+		uint8_t ReadByte(int index) override
+		{
+			base::iic::IicHostOperator op{*_iic_host};
+
+			op.Initialize(base::Nanoseconds{std::chrono::microseconds{4}},
+						  base::Nanoseconds{std::chrono::microseconds{4} * 20});
+
+			op.SendStartingSignal();
+			op.SendByte(_address_register | 0x01);
+			uint8_t data = op.ReceiveByte(true);
+			op.SendStoppingSignal();
+			return data;
+		}
 
 		/// @brief 写 1 个字节。这是 1 个 8 位的 IO 扩展芯片，写 1 个字节意味着设置所有
 		/// IO 端子的电平。
 		/// @param index 索引。要写入第几个字节。
 		/// @param value
-		void WriteByte(int index, uint8_t value) override;
+		void WriteByte(int index, uint8_t value) override
+		{
+			base::iic::IicHostOperator op{*_iic_host};
+
+			op.Initialize(base::Nanoseconds{std::chrono::microseconds{4}},
+						  base::Nanoseconds{std::chrono::microseconds{4} * 20});
+
+			op.SendStartingSignal();
+			op.SendByte(_address_register | 0x00);
+			op.SendByte(value);
+			op.SendStoppingSignal();
+			base::task::Delay(std::chrono::milliseconds{10});
+		}
 	};
 } // namespace bsp
