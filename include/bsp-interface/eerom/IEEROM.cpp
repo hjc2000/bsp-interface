52b854186032170b165fc7123821d047320ff0a6
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-31 23:20:31 +0800

    添加base::stream::Span头文件引用

--- include/bsp-interface/eerom/IEEROM.cpp
+++ include/bsp-interface/eerom/IEEROM.cpp
@@ -1,73 +1,74 @@
 #include "IEEROM.h"
+#include "base/stream/Span.h"
 
 uint16_t bsp::IEEROM::ReadUInt16(int32_t addr)
 {
-    uint16_t data;
-    Read(addr, base::Span{reinterpret_cast<uint8_t *>(&data), sizeof(data)});
-    return data;
+	uint16_t data;
+	Read(addr, base::Span{reinterpret_cast<uint8_t *>(&data), sizeof(data)});
+	return data;
 }
 
 uint32_t bsp::IEEROM::ReadUInt32(int32_t addr)
 {
-    uint32_t data;
-    Read(addr, base::Span{reinterpret_cast<uint8_t *>(&data), sizeof(data)});
-    return data;
+	uint32_t data;
+	Read(addr, base::Span{reinterpret_cast<uint8_t *>(&data), sizeof(data)});
+	return data;
 }
 
 uint64_t bsp::IEEROM::ReadUInt64(int32_t addr)
 {
-    uint64_t data;
-    Read(addr, base::Span{reinterpret_cast<uint8_t *>(&data), sizeof(data)});
-    return data;
+	uint64_t data;
+	Read(addr, base::Span{reinterpret_cast<uint8_t *>(&data), sizeof(data)});
+	return data;
 }
 
 void bsp::IEEROM::Read(int32_t addr, base::Span const &span)
 {
-    if (addr + span.Size() > Size())
-    {
-        throw std::out_of_range{"要从 EEROM 读取的数据超出储存器范围。"};
-    }
+	if (addr + span.Size() > Size())
+	{
+		throw std::out_of_range{"要从 EEROM 读取的数据超出储存器范围。"};
+	}
 
-    for (int i = 0; i < span.Size(); i++)
-    {
-        span[i] = ReadByte(addr + i);
-    }
+	for (int i = 0; i < span.Size(); i++)
+	{
+		span[i] = ReadByte(addr + i);
+	}
 }
 
 void bsp::IEEROM::Read(int32_t addr, uint8_t *buffer, int32_t offset, int32_t count)
 {
-    Read(addr, base::Span{buffer + offset, count});
+	Read(addr, base::Span{buffer + offset, count});
 }
 
 void bsp::IEEROM::WriteUInt16(int32_t addr, uint16_t data)
 {
-    Write(addr, base::ReadOnlySpan{reinterpret_cast<uint8_t *>(&data), sizeof(data)});
+	Write(addr, base::ReadOnlySpan{reinterpret_cast<uint8_t *>(&data), sizeof(data)});
 }
 
 void bsp::IEEROM::WriteUInt32(int32_t addr, uint32_t data)
 {
-    Write(addr, base::ReadOnlySpan{reinterpret_cast<uint8_t *>(&data), sizeof(data)});
+	Write(addr, base::ReadOnlySpan{reinterpret_cast<uint8_t *>(&data), sizeof(data)});
 }
 
 void bsp::IEEROM::WriteUInt64(int32_t addr, uint64_t data)
 {
-    Write(addr, base::ReadOnlySpan{reinterpret_cast<uint8_t *>(&data), sizeof(data)});
+	Write(addr, base::ReadOnlySpan{reinterpret_cast<uint8_t *>(&data), sizeof(data)});
 }
 
 void bsp::IEEROM::Write(int32_t addr, base::ReadOnlySpan const &span)
 {
-    if (addr + span.Size() > Size())
-    {
-        throw std::out_of_range{"要写入到 EEROM 的数据超出储存器范围。"};
-    }
+	if (addr + span.Size() > Size())
+	{
+		throw std::out_of_range{"要写入到 EEROM 的数据超出储存器范围。"};
+	}
 
-    for (int i = 0; i < span.Size(); i++)
-    {
-        WriteByte(addr + i, span[i]);
-    }
+	for (int i = 0; i < span.Size(); i++)
+	{
+		WriteByte(addr + i, span[i]);
+	}
 }
 
 void bsp::IEEROM::Write(int32_t addr, uint8_t const *buffer, int32_t offset, int32_t count)
 {
-    Write(addr, base::ReadOnlySpan{buffer + offset, count});
+	Write(addr, base::ReadOnlySpan{buffer + offset, count});
 }
