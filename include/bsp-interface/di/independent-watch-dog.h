09ca524423e93b4e21573780ffee38ee4ca4ac8a
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-20 23:34:30 +0800

    删除独立看门狗相关接口

--- include/bsp-interface/di/independent-watch-dog.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <bsp-interface/timer/IIndependentWatchDog.h>
-
-/// @brief 独立看门狗。
-/// @return
-bsp::IIndependentWatchDog &DI_IndependentWatchDog();
--- include/bsp-interface/timer/IIndependentWatchDog.cpp
+++
@@ -1,1 +0,0 @@
-#include "IIndependentWatchDog.h"
